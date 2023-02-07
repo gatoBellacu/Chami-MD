df327dded816bec154213f9cd7f277fbebf70c5c
Azamiwithlogic <119384234+AzamiWithLogic@users.noreply.github.com>
2023-02-07 16:04:31 -0600

    Update and rename plugins/_autoReply.js to complementos/_autoReply.js

--- complementos/_autoReply.js
+++ complementos/_autoReply.js
@@ -3,7 +3,7 @@ export async function all(m) {
 	
     // cuando alguien envía un enlace de un grupo al dm del bot
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Abre este enlace')) && !m.isBaileys && !m.isGroup) {
-     this.sendButton(m.chat, `*Invitar bot a un grupo* 
+     this.sendButton(m.chat, `*❏ Invitar el bot a su grupo ❑* 
         
   Hola @${m.sender.split('@')[0]} 
   puedes alquilar el bot para que se una a un grupo 
