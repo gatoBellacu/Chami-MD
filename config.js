73538f04dabeb38979188084c2a5cfcc40a1768f
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-09 20:39:41 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+  global.ChatOwner = "56973856790@s.whatsapp.net"
+  global.ownerNumber = "56973856790@s.whatsapp.net"
+  global.ownerNumberm = "56973856790"
+  global.botName = "Aurora - Multi Device Beta"
+  global.ownerName = "ConfuMods"
+  global.sessionName = "session"
+  global.footer = "Aurora MD-Beta"
+  global.footerbut = "Haga clic en el botón de abajo.."
+  global.apikeyOpenAI = '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'
+  global.ORG_ID = "org-79b8Y4lLwYPQoxmDhzbCx7O1"
+
+  global.linkgcbot = 'https://chat.whatsapp.com/xxxxxxxx'
+
+  global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+  global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+  global.doc4 = 'application/zip'
+  global.doc5 = 'application/pdf'
+  global.doc6 = 'application/vnd.android.package-archive'
+
+  global.youtubel = "https://youtu.be/@ConfuMods"
---
+++ help.js
@@ -0,0 +1,695 @@
+
+
+var monoSpace = '```'
+exports.listmenu = (sender,prefix,ad,namenya,premnya,usernya,romnya,readmoore,tanggal,jam,no) => {
+return`╔═══════✪ MAIN	
+╠
+╠ ${prefix}menu
+╠ ${prefix}rules
+╠ ${prefix}owner
+╠ ${prefix}infobot
+╠ ${prefix}gcbot
+╠
+╠═══════✪ USER
+╠
+╠ ${prefix}verify
+╠ ${prefix}report
+╠ ${prefix}request
+╠ ${prefix}menfess
+╠ ${prefix}buatroom
+╠ ${prefix}secretchat
+╠ ${prefix}checkprem
+╠ ${prefix}daftarprem
+╠ ${prefix}changename
+╠
+╠═══════✪ OWNER
+╠
+╠ ${prefix}error
+╠ ${prefix}clearerr
+╠ ${prefix}siaran
+╠ ${prefix}session
+╠ ${prefix}resetdb
+╠ ${prefix}runtime
+╠ ${prefix}setexif
+╠ ${prefix}setwm
+╠ ${prefix}setfooter
+╠ ${prefix}setppbot
+╠ ${prefix}addprem
+╠ ${prefix}delprem
+╠ ${prefix}join
+╠ ${prefix}bctext
+╠ ${prefix}bcvideo
+╠ ${prefix}bcaudio
+╠ ${prefix}bcimage
+╠ ${prefix}broadcast
+╠ ${prefix}sendbug 569xxx
+╠ ${prefix}philips 569xxx
+╠ ${prefix}philips2 569xxx
+╠ ${prefix}philips3 569xxx
+╠ ${prefix}santet @tag
+╠ ${prefix}santet2 @tag
+╠ ${prefix}santet3 @tag
+╠ ${prefix}virtex 569xxx
+╠ ${prefix}virtex2 569xxx
+╠ ${prefix}virtex3 569xxx
+╠ ${prefix}bug1 569xxx
+╠ ${prefix}bug2 569xxx
+╠ ${prefix}bug3 569xxx
+╠ ${prefix}bug4 569xxx
+╠ ${prefix}bug5 569xxx
+╠
+╠═══════✪ VIP
+╠
+╠ ${prefix}desactivar
+╠ ${prefix}scrapenum
+╠ ${prefix}mediafire
+╠
+╠═══════✪ ADMI
+╠
+╠ ${prefix}delete
+╠ ${prefix}revoke
+╠ ${prefix}tagall
+╠ ${prefix}hidetag
+╠ ${prefix}setdesc
+╠ ${prefix}linkgrup
+╠ ${prefix}infogroup
+╠ ${prefix}setppgrup
+╠ ${prefix}setnamegrup
+╠ ${prefix}group open
+╠ ${prefix}group close
+╠ ${prefix}antilink on
+╠ ${prefix}antilink off
+╠ ${prefix}welcome on
+╠ ${prefix}welcome off
+╠ ${prefix}tiktokauto on
+╠ ${prefix}tiktokauto off
+╠ ${prefix}kick @tag
+╠ ${prefix}demote @tag
+╠ ${prefix}promote @tag
+╠
+╠═══════✪ DESCARGAS
+╠
+╠ ${prefix}play
+╠ ${prefix}tiktok
+╠ ${prefix}ytmp3
+╠ ${prefix}ytmp4
+╠ ${prefix}pinterest
+╠ ${prefix}playmp4
+╠ ${prefix}gitclone
+╠ ${prefix}mediafire
+╠ ${prefix}soundcloud
+╠
+╠═══════✪ CREADOR
+╠ ${prefix}tts
+╠ ${prefix}ttp
+╠ ${prefix}ttp2
+╠ ${prefix}attp
+╠ ${prefix}attp2
+╠ ${prefix}tourl
+╠ ${prefix}upload
+╠ ${prefix}toimg
+╠ ${prefix}toimage
+╠ ${prefix}tomp3
+╠ ${prefix}toaudio
+╠ ${prefix}tomp4
+╠ ${prefix}tovideo
+╠ ${prefix}emojimix
+╠ ${prefix}emojmix
+╠ ${prefix}emojinua
+╠ ${prefix}mixemoji
+╠ ${prefix}stiker
+╠ ${prefix}sticker
+╠ ${prefix}sgif
+╠ ${prefix}stikergif
+╠ ${prefix}stickergif
+╠ ${prefix}swm
+╠ ${prefix}stikerwm
+╠ ${prefix}stickerwm
+╠ ${prefix}smeme
+╠ ${prefix}memestiker
+╠ ${prefix}stikermeme
+╠ ${prefix}stickermeme
+╠ ${prefix}takesticker
+╠ ${prefix}emojinua2
+╠ ${prefix}mixemoji2
+╠ ${prefix}emojmix2
+╠ ${prefix}emojimix2
+╠
+╠═══════✪ IMG EDT
+╠
+╠ ${prefix}sadcat
+╠ ${prefix}iaedit
+╠ ${prefix}gimage
+╠
+╠═══════✪ CEHCK
+╠
+╠ ${prefix}checkprem
+╠ ${prefix}checkuser
+╠
+╠═══════✪ ANONYMOUS
+╠
+╠ ${prefix}buatroom 569xxx
+╠ ${prefix}room <only owner>
+╠ ${prefix}stopchat <only room>
+╠ ${prefix}menfess 569xxx|juan|holi
+╠
+╠═══════✪ IA
+╠
+╠ ${prefix}chatgpt
+╠ ${prefix}simi
+╠ ${prefix}aurora
+╠
+╠═══════✪ STALK
+╠
+╠ ${prefix}stalkff
+╠ ${prefix}stalknpm
+╠ ${prefix}stalkgithub
+╠
+╠═══════✪ AUDIO
+╠
+╠ ${prefix}fat
+╠ ${prefix}fast
+╠ ${prefix}slow
+╠ ${prefix}bass
+╠ ${prefix}deep
+╠ ${prefix}tupai
+╠ ${prefix}robot
+╠ ${prefix}blown
+╠ ${prefix}smooth
+╠ ${prefix}earrape
+╠ ${prefix}reverse
+╠ ${prefix}nightcore
+╠
+╠═══════✪ FUN
+╠
+╠ ${prefix}checkgay
+╠ ${prefix}checklesbi
+╠ ${prefix}checkputa
+╠ ${prefix}checkidiota
+╠ ${prefix}checkurgido
+╠ ${prefix}checkcornudo
+╠
+╠═══════✪ WALLPAPER
+╠
+╠ ${prefix}loli
+╠ ${prefix}wallml
+╠ ${prefix}cosplay
+╠ ${prefix}ppcouple
+╠ ${prefix}mobil
+╠ ${prefix}wallhp
+╠ ${prefix}programing
+╠ ${prefix}wallpaperprogamer
+╠ ${prefix}wallpaperteknologi
+╠ ${prefix}wallpapercyber
+╠
+╠═══════✪ ANIME
+╠
+╠ ${prefix}cry
+╠ ${prefix}hug
+╠ ${prefix}pat
+╠ ${prefix}bully
+╠ ${prefix}lick
+╠ ${prefix}kiss
+╠ ${prefix}awoo
+╠ ${prefix}waifu
+╠ ${prefix}shinobu
+╠ ${prefix}cuddle
+╠ ${prefix}megumin
+╠ ${prefix}slap
+╠ ${prefix}neko
+╠ ${prefix}wink
+╠ ${prefix}dance
+╠ ${prefix}poke
+╠ ${prefix}glomp
+╠ ${prefix}bite
+╠ ${prefix}nom
+╠ ${prefix}handhold
+╠ ${prefix}highfive
+╠ ${prefix}wave
+╠ ${prefix}smug
+╠ ${prefix}smile
+╠ ${prefix}bonk
+╠
+╠═══════✪ NSFW
+╠
+╠ ${prefix}ass
+╠ ${prefix}bdsm
+╠ ${prefix}nloli
+╠ ${prefix}ahegao
+╠ ${prefix}blowjob
+╠
+╠═══════✪ OTROS
+╠
+╠ ${prefix}ppcp
+╠ ${prefix}cuevana
+╠ ${prefix}pelisplus
+╠ ${prefix}tranducir
+╚═════════════✪`
+}
+
+//////====================================================================/////
+//////====================================================================/////
+
+exports.rulesBot = () =>{
+return`*──「 REGLAS 」──*
+
+1. No permitido spam. 
+Sancion: *Advertencia Bloqueo*
+
+2. No permitido llamar.
+Sancion: *Bloqueo*
+
+3. No permitido insultar al bot.
+Sancion: *Bloqueo Permanente*
+
+Si quedo todo claro
+Escribe *#menu* para empezar`
+}
+
+//////====================================================================/////
+//////====================================================================/////
+
+exports.donasiBot = (cekName,ucapanWaktu) =>{
+return`──「 *DONACION* 」──
+
+NO SE ACEPTAN DONATIVOS.
+ESTE BOT NO PERMITE DONATIVOS
+BOT 100% GRATIS NO ACEPTAMOS DINERO
+SIENTETE LIBRE DE USARLO Y EDITARLO`
+}
+
+//////====================================================================/////
+//////====================================================================/////
+
+exports.infoOwner = () =>{
+return`Sin info`
+}
+
+//////====================================================================/////
+//////====================================================================/////
+
+exports.verify_teksi = (sender,user_name,res_us,thgxv) =>{
+return`───「 *VERIFICADO* 」────
+
+${thgxv}○ ID : @${sender.split('@')[0]}
+○ Nick : ${user_name}
+○ Serial : ${res_us}
+
+por favor escribe #rules
+para leer las reglas del bot${thgxv}
+
+Gracias por usarme <3
+`
+}
+
+//////====================================================================/////
+//////====================================================================/////
+
+exports.textmenu = (sender,namenya,premnya,jam,tanggal,usernya,romnya) => {
+return`𝗨𝗦𝗘𝗥 𝗜𝗡𝗙𝗢
+ID : @${sender.split('@')[0]}
+Nombre : ${namenya}
+Premium : ${premnya}
+
+𝗕𝗢𝗧 𝗜𝗡𝗙𝗢
+Libreria : Baileys-MD
+Hora : ${jam}
+Fecha : ${tanggal}
+Usuarios : ${usernya}
+Rooms : ${romnya}`
+}
+
+//////====================================================================/////
+//////====================================================================/////
+
+exports.chatgpttext = () => {
+return `𝐂𝐇𝐀𝐓 𝐆𝐏𝐓 𝐎𝐏𝐄𝐍 𝐀𝐈`
+}
+
+exports.aufnc = () => {
+return `Holi soy Aurora un bot de whatsapp. No soy un humano
+            
+Soy como dije un bot de whatsapp cuento con muchos comandos puedes verlos usando #menu
+Cuento con 2 inteligencias artificiales "Chat Gpt & Sim Simi" yo tambien estoy en proceso de ser una IA simple
+
+Por ahora las funciones que te puedo facilitar son
+
+#1 Crearte Sticker
+#2 Convertir sticker en jpg
+#3 Enviar imagenes de lolis
+#4 Decirte que anime saldra hoy
+#5 Investigar con Chat GPT
+#6 Hablar contigo utilizando Sim Simi
+
+Aun sigo en entrenamiento espero te sirva de ayuda :3 tkm<3`
+}
+
+//////====================================================================/////
+//////====================================================================/////
+
+exports.liricstxt = (lyricsa) => {
+return`*Titulo* : ${lyricsa.title}
+*Artista* : ${lyricsa.artist}
+*Letra* :
+
+${lyricsa.lyrics}`
+}
+
+exports.cuevanatxt = (q,aaaa,timepr,resultado,sipnosis,linkpl) => {
+return`🔎 Se Busco La Pelicula ${q}
+
+🎬 • Nombre: ${aaaa[0].title}
+
+🍿 • Fuente: Cuevana
+
+⏰ • Duración: ${timepr}
+
+📚 • Genero: ${resultado}
+
+📖 • Sipnosis :
+➣_${sipnosis}
+
+🍿 • Url:* ${linkpl? `https://cuevana3.mu${linkpl}`:`No se encontro enlace, esto se puede deber a que tu busqueda sea sobre una serie.`}`
+}
+
+
+exports.antilinkmg = () => {
+return`*「 Anti-Link 」*\n\n No seras expulsado porque el enlace pertenece a este grupo :D`
+}
+exports.antilinkmga = () => {
+return`*「 Anti-Link 」*\n\nUn administrador ha enviado un enlace, Los administradores son libres de enviar cualquier enlace`
+}
+exports.antilinkmsgk = (sender) => {
+return`「 ANTILINK 」\n\n@${sender.split('@')[0]} No esta permitido enviar enlaces por lo cual seras eliminado. bye bye :)`
+}
+
+
+exports.tiktokautotxt = (tt_res,chats) => {
+return`𝗧𝗜𝗞𝗧𝗢𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗
+
+Creador: ConfuMods
+Título: ${tt_res.result.judul}
+Fuente: ${chats}
+
+Se esta enviando el video...`
+}
+
+exports.packnamestk = () => {
+return`🌺 ⇝ Solicitado por:
+🌺 ⇝ Bot:
+🌺 ⇝ Dueño:`
+}
+exports.authorstk = (namensi,footer,ownerName) => {
+return`⚝ ${namensi}
+⚝ ${footer}
+⚝ ${ownerName}`
+}
+
+
+exports.roommsg = () =>{
+    return`Estás en una sala de chat, escribe *#stopchat* para eliminar la sesión de chat.`
+}
+exports.roommsgn = () =>{
+    return`*Por favor escriba el número de WhatsApp con el que desea chatear*\n\n*Ejemplo:* 56xxxxx`
+}
+exports.roommsgc = () => {
+    return`Estás en una sesión de sala de chat, esperando la confirmación del destinatario.`
+}
+exports.roommsgex = (penerimanyo) => {
+return`Envie con éxito una invitación de chat a @${penerimanyo.split('@')[0]} espera a que acepte la invitación para chatear de forma anónima para que no sepa quién eres`
+}
+exports.roommsginvit = () => {
+return`*Alguien te invita al chat*\n\n*De:* Anónimo\n\nHaga clic en el botón si desea conectarse al chat *ANÓNIMO*`
+}
+
+
+
+exports.setnametxtbot = () => {
+return`*Ok listo*\n*Por favor escribe mi nuevo nombre ^^*`
+}
+exports.setnametxtuser = (sender,cekUser,q,jam) => {
+    return`*Cambio De Nombre*
+    _*ID:* @${sender.split('@')[0]}_
+    _*Nick Viejo:* ${cekUser("name", sender)}_
+    _*Nick Nuevo:* ${q}_
+    _*Tiempo:* ${jam}_`
+}
+
+
+exports.cuturl = () => {
+return`*Envíe la URL que desea acortar*\n\n*Ejemplo:* https://google.com`
+}
+exports.cuturlmsg = (time,ii,data_deposit) => {
+    return`*ACORTADO*
+
+    *TIPO*
+    https://bitly.com/
+    
+    *TIEMPO*
+    ${time}
+    
+    *RESULTADO*
+    ${ii.result}
+    
+    *ORIGINAL* 
+    ${data_deposit.data.trannss}`
+}
+exports.cuturltini = (time,ii,data_deposit) => {
+    return`*ACORTAR*
+
+    *TIPO*
+    https://tinyurl.com/
+    
+    *TIEMPO*
+    ${time}
+    
+    *RESULTADO*
+    ${ii.result}
+    
+    *ORIGINAL* 
+    ${data_deposit.data.trannss}`
+}
+exports.cuturlcut = (time,ii,data_deposit) => {
+    return`*ACORTAR*
+
+    *TIPO*
+    https://cutt.ly/
+    
+    *TIEMPO*
+    ${time}
+    
+    *RESULTADO*
+    ${ii.result}
+    
+    *ORIGINAL* 
+    ${data_deposit.data.trannss}`
+}
+
+
+exports.usernamew = () => {
+return`Cual es el nombre de usuario?`
+}
+
+
+
+exports.npm_text = (x) => {
+return`*NPM STALK*
+Nombre : ${x.name}
+Version : ${x.version}
+Descripción : ${x.description}
+Author : ${x.author}
+Author_email : ${x.author_email}
+Ultima Publicación : ${x.last_published}
+mantenedores : ${x.maintainers}
+repositorio : ${x.repository}
+
+palabras clave : ${x.keywords}`
+}
+
+exports.textGitthub = (git) => {
+return`*STALK-GITHUB*
+Id : ${git.id}
+Login : ${git.login}
+Html_url : ${git.html_url}
+Tipo : ${git.type}
+Nombre : ${git.name}
+Localización : ${git.location}
+Bio : ${git.bio}
+RepoPublic : ${git.public_repos}
+Seguidores : ${git.followers}
+Siguiendo : ${git.following}
+Creado : ${git.created_at}
+Actualizado : ${git.updated_at}`
+}
+
+exports.stkfreem = (data_deposit,stalk_freefire) => {
+    return`*STALK FF*
+*ID:* ${data_deposit.data.id_nya}
+*Nombre de usuario:* ${stalk_freefire.nickname}`
+}
+exports.stkfree = () => {
+return`*Por favor envíe su id de free fire*`
+}
+
+
+exports.text_playmp3 = (zz) => {
+return`*YOUTUBE PLAYMP3*
+    
+    *Título:* ${zz.title}
+    *Canal:* ${zz.channel}
+    *Publicado:* ${zz.published}
+    *Views:* ${zz.views}
+    *Tipo:* audio/mp3
+    
+Disfruta de tu musica...`
+    }
+
+    exports.soundcm = () => {
+return"*Ejemplo:*\n#soundcloud https://soundcloud.com/xxxxxz"
+    }
+
+exports.soundctxt = (sdc) => {
+return`*SOUNDCLOUD DOWNLOAD*
+
+*Título:* ${sdc.result.title}
+*Duración:* ${sdc.result.duration}
+*Calidad:* ${sdc.result.quality}
+
+Enviando audio...`
+}
+
+exports.text_playmp4 = (xx) => {
+return`*[ 🌺 ] YOUTUBE PLAYMP4 [ 🌺 ]*
+
+    *Título:* ${xx.title}
+    *Canal:* ${xx.channel}
+    *Publicado:* ${xx.published}
+    *Views:* ${xx.views}
+    *Tipo:* video/mp4
+
+Disfruta de tu video...`
+}
+
+
+exports.playsrch = (zzzz) => {
+    return`*YOUTUBE PLAY*
+    
+*Título:* ${zzzz.title}
+*Canal:* ${zzzz.channel}
+*Publicado:* ${zzzz.published}
+*Views:* ${zzzz.views}`
+    }
+exports.infobott = (ownerNumber,ownerName,botName,jam,tanggal,db_user,db_menfes) => {
+return`𝗕𝗢𝗧 𝗜𝗡𝗙𝗢
+• Author : @${ownerNumber.split('@')[0]}
+• Owner : ${ownerName}
+• BotName : ${botName}
+• Librería : Baileys-MD
+• Tiempo : ${jam}
+• Fecha : ${tanggal}
+• Usuarios : ( ${("id", db_user).length} )
+• Rooms : ( ${db_menfes.length} )`
+}
+
+exports.roomtxt = (x) => {
+return`*ID ROOM ${x.id}*
+*CHAT1: @${x.a.split('@')[0]}*
+*CHAT2: @${x.b.split('@')[0]}*
+*ESTADO: ${x.state}*\n\n`
+}
+
+exports.daftarprem = (ChatOwner) =>{
+    return`*¿Quieres ser premium?*
+
+    Contacta con mi owner primero
+    @${ChatOwner.split('@')[0]}`
+}
+
+exports.checkpremtxt = (cekUser, sender) => {
+return`*CHECK PREMIUM*
+_• ID : @${cekUser("id", sender).split('@')[0]}_
+_• Estado : ${cekUser("premium", sender)? 'Si':'No'}_`
+}
+
+exports.tiktoktxt = (tt_res,q) => {
+    return`𝗧𝗜𝗞𝗧𝗢𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗
+
+    Titulo: ${tt_res.result.judul}
+    Fuente: ${q}
+    
+    Enviando video...`
+}
+
+exports.menutxtx = () => {
+    return`┌─❖
+    │ Hi 👋 
+    └┬❖ :)
+    │└────────────┈ ⳹`
+}
+
+exports.ytmp3txtx = (xx) => {
+return`*YTMP3 DOWNLOAD*
+
+*Título:* ${xx.title}
+*Canal:* ${xx.channel}
+*Publicado:* ${xx.published}
+*Views:* ${xx.views}
+*Tipo:* audio/mp3
+
+_Enviando audio..._`
+}
+
+exports.ytmp4txtx = (xx) => {
+    return`*YTMP4 DOWNLOAD*
+
+    *Título:* ${xx.title}
+    *Canall:* ${xx.channel}
+    *Publicado:* ${xx.published}
+    *Views:* ${xx.views}
+    *Tipo:* video/mp4
+    
+    _Enviando vídeo.._`
+}
+
+exports.tourl = (url,name,sizeNy) => {
+return`*Url :* ${url}\n*Nombre :* ${name}\n*Peso :* ${sizeNy}`
+}
+
+exports.infogrupo = (from,groupName,groupOwner,groupMembers,groupAdmins,isWelcome,isAntiLink,isAutoDownTT,groupDesc) => {
+    return`*INFO DEL GRUPO*
+    ✨ *ID:* ${from}
+    ➣ *Nombre:* ${groupName}
+    ✨ *Creador:* ${groupOwner}
+    ➣ *Participantes:* ${groupMembers.length}
+    ✨ *Admins:* ${groupAdmins.length}
+    ➣ *Welcome:* ${isWelcome? "✅":"❎"}
+    ✨ *Antilink:* ${isAntiLink? "✅":"❎"}
+    ➣ *Tiktok Auto:* ${isAutoDownTT? "✅":"❎"}
+    
+    ✨ *Descripción:*
+    
+    ${groupDesc}`   
+}
+
+exports.scpnum = (numver) => {
+    return`Numero: ${numver.number}
+    Prefijo: ${numver.country_prefix}
+    Codigo: ${numver.country_code}
+    Pais: ${numver.country_name}
+    Localizacion: ${numver.location? `${numver.location}`:`No se encontro.`}
+    Compañia: ${numver.carrier? `${numver.carrier}`:`No se encontro`}
+    Linea: ${numver.line_type? `${numver.line_type}`:`No se encontro.`}
+    `
+}
+
+exports.mediafiretxt = (namer,sizer,mimer,dater) => {
+    return`-----[ *MEDIAFIRE DOWNLOADER* ]-----
+
+    *Nombre* : ${namer}
+    *Peso* : ${sizer}
+    *Tipo* : ${mimer}
+    *Fecha* : ${dater}
+    
+    _Enviando archivo..._
+    `
+}
