a833e1e6c4962c34501e509e092417ef26b09358
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-11 22:47:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 
-say('Lightweight\nWhatsApp Bot', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] })
+say('Curiosity\nBot\nMD', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] })
 say(`'${name}' By @${author.name || author}`, { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
 
 var isRunning = false
@@ -32,7 +32,7 @@ function start(file) {
   setupMaster({ exec: args[0], args: args.slice(1) })
   let p = fork()
   p.on('message', data => {
-    console.log('[✅RECEIVED]', data)
+    console.log('[✅ RECIBIDO]', data)
     switch (data) {
       case 'reset':
         p.process.kill()
