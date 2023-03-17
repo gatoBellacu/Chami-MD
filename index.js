33bf1f5eafeedfadfb5dd52dd416d1e7a22c9da5
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-17 17:41:53 -0600

    Update and rename index-inicio.js to index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('THE DORRAT-BOT-MD', {
+say('CuriosityBot-MD', {
 font: 'tiny',
 align: 'center',
 colors: ['#ff8000']})
-say(`CREADO POR DIEGO-OFC`, {
+say(`CREADO POR @Azami19`, {
 font: 'console',
 align: 'center',
 colors: ['red']})
