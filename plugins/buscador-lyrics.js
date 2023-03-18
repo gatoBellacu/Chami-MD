0f87246d89e073a65f4ebb964ff7356d0e97c4d4
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-17 22:29:20 -0600

    CuriosityBotV1-MD 🌷

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 import { lyrics, lyricsv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret ojala*`
+if (!teks) throw `*⚠️ Ejemplo de uso correcto de comando: ${usedPrefix + command} beret ojala*`
 try {
 const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
 let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
@@ -10,11 +10,11 @@ title: result.author + result.title}))
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.thumbnail.genius) throw json
-let letratexto =`𝚃𝙸𝚃𝚄𝙻𝙾: *${result.title}*\n𝙰𝚄𝚃𝙾𝚁: *${result.author}*\n\n𝙻𝙴𝚃𝚁𝙰: ${result.lyrics}`.trim()
+let letratexto =`TITULO: *${result.title}*\nAUTOR: *${result.author}*\n\nLETRA: ${result.lyrics}`.trim()
 let linkresult = monospace + result.link + monospace
-conn.sendButton(m.chat, letratexto, `\n𝚄𝚁𝙻: ${linkresult}\n${wm}`, json.thumbnail.genius, [['🎵 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙰𝚄𝙳𝙸𝙾 🎵', `#play.1 ${text}`], ['🎥 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝚅𝙸𝙳𝙴𝙾 🎥', `#play.2 ${text}`]], m)
+conn.sendButton(m.chat, letratexto, `\nURL: ${linkresult}\n${wm}`, json.thumbnail.genius, [['🎵 Descargar audio 🎵', `#play.1 ${text}`], ['🎥 Descargar video 🎥', `#play.2 ${text}`]], m)
 } catch {
-await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*')}}
+await m.reply('*⚠️ Error, por favor vuelva a intentarlo*')}}
 handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
 handler.tags = ['internet']
 handler.command = /^(lirik|lyrics|lyric|letra)$/i
