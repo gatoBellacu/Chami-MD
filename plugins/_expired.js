7874cd5d57d14a86b414e7f9cf13fe078c0e0131
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-17 21:55:29 -0600

    CuriosityBotV1-MD 🌷

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -6,10 +6,10 @@ let chats = global.db.data.chats[m.chat]
 if (!chats.expired)
 return !0
 if (+new Date() > chats.expired) {
-let caption = `*EL BOT SE VA DEL GRUPO! SI QUIERE QUE VUELVA, USE EL COMANDO _#bottemporal_ PARA QUE VUELVA AL GRUPO!!*`
+let caption = `*⚠️ El bot se va del grupo! Si quiere que vuelva, use el comando #bottemporal para que vuelva al grupo!!*`
 let pp = './Menu2.jpg'
     
-await this.sendButton(m.chat, caption, wm, pp, [['𝙰𝙳𝙸𝙾𝚂', '.hastapronto']], null)
+await this.sendButton(m.chat, caption, wm, pp, [['Adios 😎', '.hastapronto']], null)
 await this.groupLeave(m.chat)
 chats.expired = null
 }}
