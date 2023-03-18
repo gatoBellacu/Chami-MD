68062fef1a84e1fd553959492be9c598b20fd06d
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-17 22:35:20 -0600

    CuriosityBotV1-MD 🌷

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 } catch {
-var update = execSync('git remote set-url origin https://github.com/DIEGO-OFC/DORRAT-BOT-MD.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/Azami19/CuriosityBotV1-MD.git && git pull')
 await m.reply(update.toString())
 }}
 handler.help = ['update']
