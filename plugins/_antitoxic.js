3468faf252459f847f63a5d7fc7eebf48a792723
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-17 21:28:07 -0600

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,26 +1,26 @@
-const toxicRegex = /puto|puta|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon/i
+const isSatir = /mierda|muertos|pesao|hijo de puta|cabron|hdp|mrd|zorra|asco|idiota|tonto|gilipollas|hijo puta|bot puta |gay|pito|pollas|muerto|dorrat|pendejo|pdj|Diego/i  // tambahin sendiri
 
-export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
+export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
         return !0
-    if (!m.isGroup)
-        return !1
-    let user = global.db.data.users[m.sender]
+    if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
-    const isToxic = toxicRegex.exec(m.text)
+    const isAntiSatir = isSatir.exec(m.text)
+
+    if (chat.antiSatir && isAntiSatir) {
+        await conn.sendButton(m.chat, `*⚠️ Insulto detectado ⚠️*`, wm, ['Desactivar Antitoxic', '#disable antitoxic'], m)
+await conn.sendButton(m.chat, ⚠️` *Te acabo de quitar 5 diamantes por insultar :)*`, wm, null, [
+['Perdon', `.say *❕ Si te has arrepentido o no sabias nada hablale a mi creador para que te devuelva los diamantes*\n\nwa.me/+524531106422`],
+['Limites', `.limit`]
+    ], m)
+        if (isBotAdmin && bot.restrict) {
+            // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+    global.db.data.users[m.sender].limit -= 5
     
-    if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
-       user.warn += 1
-       if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `⚠️ Hola *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, decir la palabra (${isToxic}) está prohibido en este bot *${user.warn}/5* advertencia`, false, { mentions: [m.sender] })
-    }
     
-    if (user.warn >= 5) {
-       user.warn = 0
-       await m.reply(`⚠️ *Hola *@${m.sender.split`@`[0]}*, superaste las 5 advertencias serás bloqueado y eliminado de este grupo*`, false, { mentions: [m.sender] })
-       user.banned = true
-       await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-       //await this.updateBlockStatus(m.sender, 'block')
+        } else if (!bot.restrict) return m.reply('Gk bisa gw kick!')
     }
-    return !1
+    return !0
 }
+
