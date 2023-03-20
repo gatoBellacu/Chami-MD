01075014552f41687676be80e41bf2be89d0c76e
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-20 14:06:18 -0600

    CuriosityBotV1-MD 🌷

--- plugins/_wel&bye.js
+++ plugins/_wel&bye.js
@@ -3,11 +3,11 @@ let stikerwelgc = "./storage/welgc.webp"
 let stikerbyegc = "./storage/byegc.webp"
 if (command == 'welcomegc') {
 conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'CutiosityBot-MD', body: '@Azami19', sourceUrl: `https://github.com/Azami19/CuriosityBotV1-MD`, thumbnail: imagen1}}})  
+contextInfo: { externalAdReply: { title: 'CutiosityBot-MD', body: '@Azami19', sourceUrl: `https://github.com/Azami19/CuriosityBotV1-MD`, thumbnail: img1}}})  
 }
 if (command == 'byegc') {
 conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'CuriosityBot-MD', body: '@Azami19', sourceUrl: `https://github.com/Azami19/CuriosityBotV1-MD`, thumbnail: imagen1}}})
+contextInfo: { externalAdReply: { title: 'CuriosityBot-MD', body: '@Azami19', sourceUrl: `https://github.com/Azami19/CuriosityBotV1-MD`, thumbnail: img1}}})
 }}
 handler.command = ['welcomegc', 'byegc']
 export default handler
