7ac13cdc0acb8a06c7872d4f88f8882b33e50b60
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-20 14:14:44 -0600

    CuriosityBotV1-MD 🌷

--- plugins/buscador-playstore.js
+++ plugins/buscador-playstore.js
@@ -1,16 +1,16 @@
 import fetch from 'node-fetch' 
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-    if (!text) throw `*_*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙿 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁*._*`
+    if (!text) throw `*⚠️ Ingrese el nombre de la app que quiera buscar*`
     try {
     let res = await fetch(`https://api.akuari.my.id/downloader/apkdownloader?query=${text}`)
     let json = await res.json()
     let { version, updated, developer, id, requirements, installed } = json.info
     let pp = await (await fetch('https://telegra.ph/file/e867ad919a98764a4d719.jpg')).buffer()
-    let info = `*📦 • Versión:* ${version}\n*📅 • Actualización:* ${updated}\n*👑 • Desarrollador:* ${developer}\n*ℹ️ • ID:* ${id}\n*📱 • Android:* ${requirements}\n*🛡️ • Instalada:* ${installed}`
-    await conn.sendNyanCat(m.chat, `${info}\n\n${global.wait}`, pp, `• Downloader Play Store 💎`, wm, 'https://github.com/DIEGO-OFC/DORRAT-BOT-MD', m)
+    let info = `*📦 • Versión:* ${version}\n*📅 • Actualización:* ${updated}\n*👑 • Desarrollador:* ${developer}\n*🆔 • ID:* ${id}\n*📱 • Android:* ${requirements}\n*🛡️ • Instalada:* ${installed}`
+    await conn.sendNyanCat(m.chat, `${info}\n\n${global.wait}`, pp, `• Downloader Play Store 💎`, wm, 'https://github.com/Azami19/CuriosityBotV1-MD', m)
     
 conn.sendMessage(m.chat, { document: { url: `${json.apkdownload}` }, mimetype: 'application/videos.android.package-archive', fileName: `${text}.apk` }, { quoted: m })
-} catch { m.reply('⚠️ *_Resultados no encontrados._*') }
+} catch { m.reply('⚠️ *Resultados no encontrados.*') }
 }
 handler.help = ['apkdl *<nombre de apk>*']
 handler.tags = ['downloader']
