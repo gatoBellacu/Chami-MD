f3f2ae70245c428eb31b2e0d6eedc3aad762d250
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-23 13:37:36 -0600

    Delete plugins directory

--- plugins/alias.js
+++
@@ -1,4 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command, args }) => {
-}
-handler.alias = ['tiktok', 'tikdl', 'tiktokdl', 'tiktoknowm', 'menu', 'menj', 'tu madre es puta', 'random', 'play', 'registrar', 'sticker', 'top', 'hide']
-export default handler
