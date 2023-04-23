dd6d7444e1b8966c490eca7d407a966fca085d14
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-23 14:00:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -393,5 +393,5 @@ let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s) ', s, ' Segundo(s) '].map(v => v.toString().padStart(2, 0)).join('')}
 _quickTest()
-.then(() => conn.logger.info(`Ƈᴀʀɢᴀɴᴅᴏ．．．\n`))
+.then(() => conn.logger.info(`☁️ ACTIVANDO BOT, POR FAVOR ESPERE UN MOMENTO`))
 .catch(console.error)
