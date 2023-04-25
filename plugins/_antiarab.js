75860e5c96f476fa6f27ac87f3c7ec50e1a095b5
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-24 22:08:21 -0600

    Update _antiarab.js

--- plugins/_antiarab.js
+++ plugins/_antiarab.js
@@ -6,19 +6,19 @@ let bot = global.db.data.settings[conn.user.jid] || {}
 if (isBotAdmin && chat.antiArab && !isAdmin && !isOwner && !isROwner && bot.restrict) {
 		
 if (m.sender.startsWith('212' || '212')) {
-m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`)
+m.reply(`*⚠️ En este grupo no se permiten árabes, serás eliminado*\n\n*⚠️ العرب غير مسموح في هذه المجموعة ، سيتم إزالتك*`)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return 
 }
    
 if (m.sender.startsWith('265' || '265')) {
-m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`)
+m.reply(`*⚠️ En este grupo no se permiten árabes, serás eliminado*\n\n*⚠️ غير مسموح للعرب في هذه المجموعة سوف يتم إقصاؤكم*`)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return   
 } 
 	
 if (m.sender.startsWith('92' || '92')) {
-m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`)
+m.reply(`*⚠️ En este grupo no se permiten árabes, serás eliminado\n\n* ⚠️ غير مسموح للعرب في هذه المجموعة سوف يتم إقصاؤكم*`)
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return   
 } 	
