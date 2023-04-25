62439e8b5724b4436eb0ba8681eaa9d8e1921c49
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-24 22:37:41 -0600

    Update enable_disable-antilink2.js

--- plugins/enable_disable-antilink2.js
+++ plugins/enable_disable-antilink2.js
@@ -17,12 +17,12 @@ if (m.text.includes(linkThisGroup)) return !0
 if (m.text.includes(linkThisGroup2)) return !0
 if (m.text.includes(linkThisGroup3)) return !0
 }    
-await conn.sendButton(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n*${await this.getName(m.sender)} rompiste las reglas del grupo, serás eliminado*${isBotAdmin ? '' : '\n\n*⚠️ No soy admin, no puedo eliminar el intruso*'}`, wm2, ['Desactivar antilink2', '/disable antilink'], m)    
+await conn.sendButton(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n\n*${await this.getName(m.sender)} rompiste las reglas del grupo, serás eliminado*${isBotAdmin ? '' : '\n\n*⚠️ No soy admin, no puedo eliminar el intruso*'}`, wm2, ['Desactivar antilink2', '/disable antilink'], m)    
 if (isBotAdmin && bot.restrict) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return
-} else if (!bot.restrict) return m.reply('*Las restricciones no están activas por mi creador*')
+} else if (!bot.restrict) return m.reply('*⚠️ Las restricciones no están activas por mi creador*')
 }
 return !0
 }
