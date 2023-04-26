3e203e24146b37ae0c21a69de7b6f0205e4a06bd
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-26 14:09:30 -0600

    Update owner-broadcastchats.js

--- plugins/owner-broadcastchats.js
+++ plugins/owner-broadcastchats.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
   conn.reply(m.chat, `Enviar transmisiones a ${chats.length} chat_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + author + ' All Chat Broadcast 」\n' + randomID(32)), true).catch(_ => _)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + wm + ' All Chat Broadcast 」\n' + randomID(32)), true).catch(_ => _)
   m.reply('Después de transmitir todo el chat:')
 }
 handler.help = ['broadcastchats', 'bcchats'].map(v => v + ' <teks>')
