f19a953c060d87472edaeeffb129c1364f5117d9
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-26 13:16:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone' 
 
-/*============ OWNER ============*\
+//============ OWNER ============
 global.owner = [
   ['5214531106422', '🍥 Creador 🍥', true],
   ['59894808483', '🎁 desarrollador 🎁', true],
