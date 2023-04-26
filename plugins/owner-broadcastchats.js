813e2e5516eeab6cb47b3a12047aa0067b479222
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-26 17:53:41 -0300

    Update owner-broadcastchats.js

--- plugins/owner-broadcastchats.js
+++ plugins/owner-broadcastchats.js
@@ -6,6 +6,7 @@ let handler = async (m, { conn, text }) => {
   let teks = text ? text : cc.text
   await delay(5 * 5000)
   conn.reply(m.chat, `*⚠️ Enviando mensaje a ${chats.length} chats*`, m)
+  await delay(5 * 5000)
   for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + wm + ' 」\n' + randomID(5)), true).catch(_ => _)
   m.reply('*⚠️ Termine de enviar el mensaje a todos los chats*')
 }
@@ -21,4 +22,6 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
