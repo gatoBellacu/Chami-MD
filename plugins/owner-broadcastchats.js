bad5e3a8e9ed8e04dbf47a49988bc98c010ca159
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-26 14:20:08 -0600

    Update owner-broadcastchats.js

--- plugins/owner-broadcastchats.js
+++ plugins/owner-broadcastchats.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `Enviar transmisiones a ${chats.length} chat_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + wm + ' All Chat Broadcast 」\n' + randomID(32)), true).catch(_ => _)
-  m.reply('Termine de transmitir todos los chats:')
+  conn.reply(m.chat, `*⚠️ Enviando mensaje a ${chats.length} chats`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + wm + ' 」\n' + randomID(32)), true).catch(_ => _)
+  m.reply('*⚠️ Termine de enviar el mensaje a todos los chats*')
 }
 handler.help = ['broadcastchats', 'bcchats'].map(v => v + ' <teks>')
 handler.tags = ['owner']
