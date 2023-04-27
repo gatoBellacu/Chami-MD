b24b03bcba93129156ac1c54b7a89a30e68536df
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-26 20:40:22 -0600

    Update enable_disable-antiviewonce.js

--- plugins/enable_disable-antiviewonce.js
+++ plugins/enable_disable-antiviewonce.js
@@ -8,6 +8,7 @@ if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return
 if (!chat.antiviewonce || chat.isBanned) return
 if (m.mtype == 'viewOnceMessageV2') {
 let msg = m.message.viewOnceMessageV2.message
+let type = Object.keys(msg)[0]
 let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
 let buffer = Buffer.from([])
 for await (const chunk of media) {
