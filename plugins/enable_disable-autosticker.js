a3d164b515ee49f35929f7218525a88d7990acd2
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-27 06:41:28 -0600

    Update enable_disable-autosticker.js

--- plugins/enable_disable-autosticker.js
+++ plugins/enable_disable-autosticker.js
@@ -19,7 +19,7 @@ if (!img) return
 stiker = await createSticker(img, false, packname || global.packname, author || global.author)
 //stiker = await sticker(img, false, packname, author)
 } else if (/video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return await this.sendButton(m.chat, '*⚠️ El vídeo no puede durar más de 10 segundos*', wm, [['Desactivar autosticker', '/disable autosticker']], m)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return await this.sendButton(m.chat, '*⚠️ El vídeo no puede durar más de 10 segundos*', wm2, [['Desactivar autosticker', '/disable autosticker']], m)
 let img = await q.download()
 if (!img) return
 stiker = await mp4ToWebp(img, { pack: packname || global.packname, author: author || global.author })
