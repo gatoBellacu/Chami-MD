f4cf4db0b4412189f7f637c530b096987118af72
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-26 22:00:48 -0600

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
 let user = global.db.data.users[m.sender]
 user.afk = + new Date
 user.afkReason = text
-m.reply(`*「 🚀 CURIOSITY AFK 🚀 」*\n\n*⚠️ El usuario ${conn.getName(m.sender)} estará inactivo, por favor no lo vayan a etiquetar*\n\n*📍 Motivo de inactividad: ${text ? ': ' + text : ''}*
+m.reply(`*「 🚀 CURIOSITY AFK 🚀 」*\n\n*⚠️ El usuario ${conn.getName(m.sender)} estará inactivo, por favor no lo vayan a etiquetar*\n\n*📍 Motivo de inactividad: ${text ? ':' + text : ''}*
 `)}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
