9b199f9a58dc4687dab9f00b6fe2fbd5fab180b1
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-26 22:21:14 -0600

    Update config.js

--- config.js
+++ config.js
@@ -450,7 +450,7 @@ global.mes = d.toLocaleDateString('es', { month: 'long' })
 global.año = d.toLocaleDateString('es', { year: 'numeric' })
 global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 //*****************************
-global.wm2 = `▸ 📅 ${dia}\n▸ 📆 ${fecha}\n▸ 📊 ${mes}\n▸ 🚀 ${año}\n▸ ⏰${tiempo}\n▸ *🔰 CuriosityBot-MD 🔰*`
+global.wm2 = `▸ 🗓️ ${dia}\n▸ 📆 ${fecha}\n▸ 📊 ${mes}\n▸ 🚀 ${año}\n▸ ⏰${tiempo}\n▸ *🔰 CuriosityBot-MD 🔰*`
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf']
 global.cmenut = '❖––––––『'
 global.cmenub = '┊✦ '
