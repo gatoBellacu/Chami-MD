9d165d8b66b791c742c1f108e77f4750b2d02d79
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-29 13:34:51 -0600

    Update downloader-imagen.js

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -1,6 +1,5 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 if (!text) throw `*⚠️ Ejemplo de uso de comando ${usedPrefix + command} Robot Curiosity*`
 const res = await googleImage(text)
 let image = await res.getRandom()
