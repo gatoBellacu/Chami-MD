46aac64b6ec8cc4b369342408d2c70d4d98eda37
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-29 09:33:57 -0600

    Update cmd-add.js

--- plugins/cmd-add.js
+++ plugins/cmd-add.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 global.db.data.sticker = global.db.data.sticker || {}
 if (!m.quoted) throw '*⚠️ Responde a una imagen o a un sticker el cuál se le agregara un comando o texto según usted decida*'
-if (!m.quoted.fileSha256) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚂𝙾𝙻𝙾 𝙿𝚄𝙴𝙳𝙴𝚂 𝙰𝚂𝙸𝙶𝙰𝙽𝙰𝚁 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝙾 𝚃𝙴𝚇𝚃𝙾𝚂 𝙰 𝚂𝚃𝙸𝙲𝙺𝙴𝚁𝚂 𝙴 𝙸𝙼𝙰𝙶𝙴𝙽𝙴𝚂*'
+if (!m.quoted.fileSha256) throw '*⚠️ Solo puedes agregar comandos o textos a stickers e imagenes*'
 if (!text) throw `*⚠️ Fallo de uso, texto faltante*\n\n*📍 Uso correcto de comando:*\n*✅ ${usedPrefix + command} <texto> <responder a sticker o imagen>*\n\n*🚀 Ejemplo de uso correcto:*\n*✅ ${usedPrefix + command} <#menu> <responder a sticker o imagen>*`
 let sticker = global.db.data.sticker
 let hash = m.quoted.fileSha256.toString('base64')
 if (sticker[hash] && sticker[hash].locked) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚂𝙾𝙻𝙾 𝙴𝙻 𝙾𝚆𝙽𝙴𝚁 𝙿𝚄𝙴𝙳𝙴 𝚁𝙴𝙰𝙻𝙸𝚉𝙰𝚁 𝙻𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙲𝙸𝙾𝙽*'
 sticker[hash] = { text, mentionedJid: m.mentionedJid, creator: m.sender, at: + new Date, locked: false }
-m.reply(`*[ ✔ ] 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾/𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙰𝚂𝙸𝙶𝙽𝙰𝙳𝙾 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁/𝙸𝙼𝙰𝙶𝙴𝙽 𝙵𝚄𝙴 𝙰𝙶𝚁𝙴𝙶𝙰𝙳𝙾 𝙰 𝙻𝙰 𝙱𝙰𝚂𝙴 𝙳𝙴 𝙳𝙰𝚃𝙾𝚂 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴*`)
+m.reply(`*✅ El texto/comando fue agregado al sticker/imagen y se encuentra agregado en la base de datos*`)
 }
 handler.command = ['setcmd', 'addcmd', 'cmdadd', 'cmdset']
 handler.rowner = true
