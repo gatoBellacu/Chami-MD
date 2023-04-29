7dd40e91bdd21ce5ede10f95938b0c55a07213ff
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-04-29 09:47:35 -0600

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 conn.reply(m.chat, `
 *🚀 LISTA CMD 🚀*
 
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*📍 CODIGO:* ${value.locked ? `*(❌ bloqueado)* ${key}` : key}\n*✅ TEXTO/COMANDO* ${value.text}`).join('\n\n')}
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*📍 CODIGO:* ${value.locked ? `*(❌ bloqueado)* ${key}` : key}\n*✅ TEXTO/COMANDO:* ${value.text}`).join('\n\n')}
 `.trim(), null, {mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
 }
 handler.command = ['listcmd', 'cmdlist']
