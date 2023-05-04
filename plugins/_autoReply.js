3e3050da814c70156308a0f32717a31a2de529cd
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-04 13:58:53 -0600

    Add files via upload

---
+++ plugins/_autoReply.js
@@ -0,0 +1,16 @@
+
+export async function all(m) {
+	
+    // cuando alguien envía un enlace de un grupo al dm del bot
+    if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Abre este enlace')) && !m.isBaileys && !m.isGroup) {
+     this.sendButton(m.chat, `*❏ INVITAR BOT A SU GRUPO ❑* 
+        
+  Hola @${m.sender.split('@')[0]} 
+  puedes invitar el bot para que se una a un grupo 
+  más info click en el botón
+`.trim(), fgig, null, [['✅ INVITAR ', '.buyprem']] , m, { mentions: [m.sender] })
+    m.react('🔥')
+  } 
+  
+   return !0
+}
