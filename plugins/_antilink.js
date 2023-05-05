7bc863cb3a1e89f9b953a616b0a9d440adf7664c
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-05 11:39:15 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,28 +1,28 @@
+import db from '../lib/database.js'
 
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 
-export async function before(m, {conn, isAdmin, isBotAdmin }) {
+export async function before(m, { conn, isAdmin, isBotAdmin, usedPrefix }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
+    let chat = db.data.chats[m.chat]
+    let bot = db.data.settings[this.user.jid] || {}
+    let prt = m.key.participant
+    let yid = m.key.id
     const isGroupLink = linkRegex.exec(m.text)
-
     if (chat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) return !0
+            if (m.text.includes(linkThisGroup)) return m.reply('*⚠️ Acabas de enviar un enlace, pero el enlace es de este grupo*')
         }
-        await conn.reply(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*
-
-*🔰 El antilink esta activo 🔰*
-
-*😼 @${m.sender.split('@')[0]} rompiste las reglas serás eliminado del grupo* ${isBotAdmin ? '' : '\n\n*⚠️ Te salvaste de mi maldito, no soy admin no te puedo eliminar*'}`, null, { mentions: [m.sender] } )
-        if (isBotAdmin && chat.antiLink) {
-        	await conn.sendMessage(m.chat, { delete: m.key })
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!chat.antiLink) return //m.reply('')
+        if (!isBotAdmin) return conn.sendButton(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n`, `*Por suerte no soy admin, asi que no puedo hacer nada :v*`, wm, ['DESACTIVAR ANTILINK ', usedPrefix+'apagar antilink'], m)
+        await  conn.sendMessage(m.chat, { text: `*[ ! ] Enlace detectado [ ! ]*\n_Ahora si k_gaste @${m.sender.split("@")[0]} Adios..._\n`, mentions: [m.sender] }) 
+        conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: yid, participant: prt }})
+setTimeout(() => { 
+	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+}, 1000)
     }
     return !0
 }
+
