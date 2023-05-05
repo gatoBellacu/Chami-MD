43dfea14bf13c0c49b730e0a8acd3dcd16e6468b
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-04 19:38:08 -0600

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -17,6 +17,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
         await conn.reply(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*
 
 *🔰 El antilink esta activo 🔰*
+
 *😼 @${m.sender.split('@')[0]} rompiste las reglas del grupo serás expulsado del grupo* ${isBotAdmin ? '' : '\n\n*Te salvaste de mi maldito, no soy admin no te puedo expulsar 😆*'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
