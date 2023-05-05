be3d7a3eddaf804230d522bde13e467d5c95219b
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-05 15:45:28 -0300

    Add files via upload

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -2,7 +2,9 @@
 let handler = m => m
 handler.all = async function (m) {
 	let setting = global.db.data.settings[this.user.jid]
-	
+
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
 let _muptime
     if (process.send) {
       process.send('uptime')
@@ -12,7 +14,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `🎉 Soy ${wm} 🤖 || ⏰ Activo durante ${muptime} || 🎨 Creado por ${nameowner}`
+		let bio = `<[ 🎉 Soy ${wm} ]> || ⏰ Activo durante ${muptime} || 👥Total del usuarios: ${Object.keys(global.db.data.users).length}`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
@@ -24,5 +26,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' Día(s) ️☀️\n', h, ' Hora(s) 🕛\n', m, ' Minuto(s) ⏱️\n'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s)'].map(v => v.toString().padStart(2, 0)).join('')
 }
