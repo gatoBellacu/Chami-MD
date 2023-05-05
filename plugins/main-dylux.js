b1fedb7eeb6a9b5b315f36258d4765f3720e24a3
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-04 20:04:36 -0600

    Update main-dylux.js

--- plugins/main-dylux.js
+++ plugins/main-dylux.js
@@ -2,8 +2,7 @@ let handler = async (m, { conn }) => {
 let msg = `
 *👋 Hola @${m.sender.split`@`[0]} aquí tienes los grupos oficiales de CuriobugBot-MD.*
 
- Mmm sin diseño XD 
- `
+ Mmm sin diseño XD`
 conn.sendPayment(m.chat, '99999999', msg, m)
 }
 handler.command = handler.help = ['grupos']
