d07fe7d391b64004592da72f329f5908e13e6f39
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-04 18:42:25 -0600

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -24,5 +24,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' Día(s) ️☀️', h, ' Hora(s) ', m, ' Minuto(s)'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' Día(s) ️☀️', h, ' Hora(s) 🕛', m, ' Minuto(s) ⏱️'].map(v => v.toString().padStart(2, 0)).join('')
 }
