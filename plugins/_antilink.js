8382b6b68f17fe74ea720a72eaf68c8f7305470f
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-05 15:55:31 -0300

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,23 +1,19 @@
-import db from '../lib/database.js'
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-
-export async function before(m, { conn, isAdmin, isBotAdmin, usedPrefix }) {
+export async function before(m, {conn, isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
-    let chat = db.data.chats[m.chat]
-    let bot = db.data.settings[this.user.jid] || {}
-    let prt = m.key.participant
-    let yid = m.key.id
+    let chat = global.db.data.chats[m.chat]
+    let bot = global.db.data.settings[this.user.jid] || {}
     const isGroupLink = linkRegex.exec(m.text)
     if (chat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return m.reply('[ ! ] Aea causa acabas de enviar un enlace, lo weno es que el enlace detectado es de este grupo owo')
         }
-        if (!isBotAdmin) return conn.sendButton(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n`, `*⚠️ Por suerte no soy admin, asi que no puedo hacer nada :v*`, wm, ['Desactivar Antilink', usedPrefix+'apagar antilink'], m)
-        await  conn.sendMessage(m.chat, { text: `*⚠️ ENLACE DETECTADO ⚠️*\n\n*Valiste vrg @${m.sender.split("@")[0]} Adiós*\n`, mentions: [m.sender] }) 
+        if (!isBotAdmin) return conn.sendButton(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n`, `Por suerte no soy acmin, asi que no puedo hacer nada :v`, wm, ['[ DESACTIVAR ANTILINK ]', usedPrefix+'disable antilink'], m)
+        await  conn.sendMessage(m.chat, { text: `*⚠️ ENLACE DETECTADO ⚠️*\n\n*Valiste vrg @${m.sender.split("@")[0]} Adios..._\n`, mentions: [m.sender] }) 
         conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: yid, participant: prt }})
 setTimeout(() => { 
 	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
