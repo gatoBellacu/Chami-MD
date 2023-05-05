d37f748a852f0910d105892cf2d96ce8a8cde26d
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-04 20:03:24 -0600

    Update main-dylux.js

--- plugins/main-dylux.js
+++ plugins/main-dylux.js
@@ -1,35 +1,11 @@
-
 let handler = async (m, { conn }) => {
+let msg = `
+*👋 Hola @${m.sender.split`@`[0]} aquí tienes los grupos oficiales de CuriobugBot-MD.*
 
-m.reply(`
-≡  *DyLuxᴮᴼᵀ ┃ SUPPORT*
-
-◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ Grupo *1*
-https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU
-
-▢ Grupo *2*
-https://chat.whatsapp.com/CDUqNRu5Kh5KY5uqQI0BKE
-
-▢ Grupo *NSFW* 🔞
-https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH
-
-◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ Todos los Grupos
- https://instabio.cc/fg98ff
-
-▢ *Telegram*
-• https://t.me/fgawgp
-
- ▢ *PayPal*
-• https://paypal.me/fg98f
-
-▢ *YouTube*
-• https://www.youtube.com/fg98f`)
-
+ Mmm sin diseño XD 
+ `
+conn.sendPayment(m.chat, '99999999', msg, m)
 }
-handler.help = ['support']
-handler.tags = ['main']
-handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux', 'support'] 
-
+handler.command = handler.help = ['gruposofc']
+handler.tags = ['info']
 export default handler
