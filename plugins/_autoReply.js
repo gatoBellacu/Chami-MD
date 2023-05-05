15df43d1a7f154703d65721267282d934f511081
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-04 20:29:12 -0600

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -6,8 +6,7 @@ export async function all(m) {
      this.sendButton(m.chat, `*❏ INVITAR BOT A SU GRUPO ❑* 
         
  👋🏻 Hola @${m.sender.split('@')[0]}
-  *puedes invitar el bot para que se una a un grupo
-  más info click en el botón*
+  *puedes invitar el bot para que se una a un grupo más info click en el botón*
 `.trim(), fgig, null, [['✅ INVITAR ', '.buyprem']] , m, { mentions: [m.sender] })
     m.react('🔰')
   } 
