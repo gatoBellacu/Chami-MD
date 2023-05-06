b9a21e855e7f171ef71f72182bf4c2523420d856
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-05 18:21:56 -0600

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -13,7 +13,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `<[ 🎉 Soy ${wm} ]> || ⏰ Activo durante ${muptime} || 👥 Total del usuarios: ${Object.keys(global.db.data.users).length}`
+		let bio = `🎉 Soy ${wm} || ⏰ Activo durante ${muptime} || 👥 Total del usuarios: ${Object.keys(global.db.data.users).length} || 🎨 Creado por ${nombreowner}`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
