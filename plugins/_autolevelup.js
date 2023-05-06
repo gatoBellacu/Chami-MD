bea53f12986a79ee1d7b4f2359d0ef88397d08ff
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-06 15:48:50 -0300

    Add files via upload

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -7,16 +7,18 @@ export async function before(m, { conn }) {
         return !0
     let before = user.level * 1
     let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
     while (canLevelUp(user.level, user.exp, global.multiplier))
         user.level++
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
-    conn.sendButton(m.chat, `*🍧 SUBISTE DE NIVEL 🍧*
-
+    conn.sendButton(m.chat, `*🍧 NUEVO NIVEL 🍧*
+    
+ ${taguser} subiste de nivel
  *[ ${before} ]* ‣ *[ ${user.level} ]*
  🔰 Rango : *${user.role}*
  
  _⚠️ para desactivar escribe_
-_*/off autolevelup*_ׂ`, null, [['🍥 MENU 🍥', `/menu`]], fkontak, m)
+_*/off autolevelup*_ׂ`, null, [['✳️MENU', `/menu`]], fkontak, m)
     }
 }
