027140f3152da1727067335140dd9b429c23b613
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-06 13:47:21 -0600

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -6,8 +6,8 @@ let handler = async (m, { text, conn }) => {
     user.afkReason = text
     m.reply(`
    *「 🚀 CURIOSITY AFK 」* 
-▢ *Usuario:* ${conn.getName(m.sender)} 
-▢ *Razon:* ${text ? text : ''}
+🔰 *Usuario:* ${conn.getName(m.sender)} 
+📍 *Razon:* ${text ? text : ''}
   `)
 conn.fakeReply(m.chat, '*⚠️ Si mandas un mensaje dejaras de estar afk automáticamente*', '0@s.whatsapp.net', '*🍧 CuriosityBot-MD 🍧*', 'status@broadcast')
 }
