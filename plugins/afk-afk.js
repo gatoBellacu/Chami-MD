002b347f48e56883c54f824c15b26e804d3f04a0
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-06 13:09:55 -0600

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -6,11 +6,10 @@ let handler = async (m, { text, conn }) => {
     user.afkReason = text
     m.reply(`
    *「 🚀 CURIOSITY AFK 」* 
-Ahora estas afk hasta que envies un mensaje 
 ▢ *Usuario:* ${conn.getName(m.sender)} 
 ▢ *Razon:* ${text ? text : ''}
   `)
-conn.fakeReply(m.chat, wm, '0@s.whatsapp.net', '*🍧 CuriosityBot-MD 🍧*', 'status@broadcast')
+conn.fakeReply(m.chat, *⚠️ Si mandas un mensaje dejaras de estar afk automáticamente*, '0@s.whatsapp.net', '*🍧 CuriosityBot-MD 🍧*', 'status@broadcast')
 }
 handler.help = ['afk <razon>']
 handler.tags = ['fun']
