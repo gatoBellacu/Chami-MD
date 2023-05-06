3427f2f98bb6ef03700b4e41a9c48e44fb3c780e
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-06 15:13:12 -0600

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -1,5 +1,38 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let teks = `
+┌─「 Donasi • Pulsa 」
+│ • *Apoyar:* No tengo paypal
+❏────
+`
 
-let handler = async(m, { conn, usedPrefix, command }) => {
+let you = flaaa.getRandom()
+
+const buttons = [
+  {buttonId: '.? all', buttonText: {displayText: 'menu'}, type: 1},
+  {buttonId: '.ping', buttonText: {displayText: 'velocidad'}, type: 1},
+  {buttonId: '.creator', buttonText: {displayText: 'creador'}, type: 1}
+]
+
+const templateMessage = {
+    image: {url: you + 'Donasi'},
+    caption: teks, 
+    footer: wm,
+    buttons: buttons,
+    headerType: 4
+}
+await conn.sendMessage(m.chat, templateMessage, m)}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^donar(donasi|apoyo)$/i
+
+export default handler
+
+
+
+
+
+
+/*let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 si quieres ayudar puede donar un Número para bot y tenga las siguientes recompensas`
@@ -11,4 +44,4 @@ handler.help = ['donate']
 handler.tags = ['main']
 handler.command = ['apoyar', 'donate', 'donar'] 
 
-export default handler
+export default handler*/
