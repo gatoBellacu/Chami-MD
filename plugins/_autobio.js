4120a2077aa63196854b9a9c25068e84f5cfe6c2
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-05 18:06:30 -0600

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,4 +1,3 @@
-
 let handler = m => m
 handler.all = async function (m) {
 	let setting = global.db.data.settings[this.user.jid]
@@ -14,7 +13,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `<[ 🎉 Soy ${wm} ]> || ⏰ Activo durante ${muptime} || 👥Total del usuarios: ${Object.keys(global.db.data.users).length}`
+		let bio = `<[ 🎉 Soy ${wm} ]> || ⏰ Activo durante ${muptime} || 👥 Total del usuarios: ${Object.keys(global.db.data.users).length}`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
@@ -26,5 +25,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s)'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, 'Día(s) ️☀️\n', h, ' Hora(s) ⏱️\n', m, 'Minuto(s) ⏱️\n'].map(v => v.toString().padStart(2, 0)).join('')
 }
