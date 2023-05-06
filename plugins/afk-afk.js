7e88bd787d4eb7404ad2bffa156791da2d01b118
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-06 16:17:46 -0300

    Add files via upload

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -9,11 +9,12 @@ let handler = async (m, { text, conn }) => {
 ▢ *Usuario:* ${conn.getName(m.sender)} 
 ▢ *Razon:* ${text ? text : ''}
   `)
-conn.fakeReply(m.chat, fgsc, '0@s.whatsapp.net', '*🍧 CuriosityBot-MD 🍧*', 'status@broadcast')
+conn.fakeReply(m.chat, '*⚠️ Si mandas un mensaje dejaras de estar afk automáticamente*', '0@s.whatsapp.net', '*🍧 CuriosityBot-MD 🍧*', 'status@broadcast')
 }
 handler.help = ['afk <razon>']
 handler.tags = ['fun']
 handler.command = ['afk']
 handler.group = true
+handler.register = true
 
 export default handler
