344772f02b644001b3d80288deee5c188de2e638
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 16:12:47 -0600

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,9 +1,9 @@
 
 let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
-*LISTA CMD*
+*🍧 LISTA CMD 🍧*
 \`\`\`
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n')}
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n\n')}
 \`\`\`
 `.trim(), null, {
         mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
@@ -21,24 +21,3 @@ export default handler
 
 
 
-/*//import db from '../lib/database.js'
-
-let handler = async (m, { conn }) => {
-    conn.reply(m.chat, `
-*🍧 LISTA DE  COMANDOS 🍧*
-
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n\n')}
-
-*🔔 INFO 🔔:* Si esta en *negrita*  esta bloqueado
-
-`.trim(), null, {
-        mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
-    })
-}
-
-
-handler.help = ['listcmd']
-handler.tags = ['cmd']
-handler.command = ['listcmd']
-
-export default handler*/
