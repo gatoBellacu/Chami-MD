bbd97ae2798fdd4bc5e886f976b825c278a5132e
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-07 19:35:15 -0300

    Add files via upload

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,16 +1,10 @@
 let handler = async (m, { conn }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    conn.reply(m.chat, `
-*🍧 LISTA CMD 🍧*
+conn.sendButton(m.chat, `*🍧 LISTA CMD 🍧*
 \`\`\`
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n\n')}
-\`\`\`
-`.trim(), null, {
-        mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
-    })
+\`\`\``, null, [['✳️MENU', `/menu`]], fkontak, m)
 }
-
-
 handler.help = ['listcmd']
 handler.tags = ['database']
 handler.command = ['listcmd']
