7458be4873a1540f6d7545b634e53a9785d5addc
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 15:41:36 -0600

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -4,10 +4,9 @@ let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
 *🍧 LISTA DE  COMANDOS 🍧*
 
-📍 *🔔 INFO 🔔:* Si esta en *negrita*  esta bloqueado
-
-──────────────────
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n\n')}
+
+*🔔 INFO 🔔:* Si esta en *negrita*  esta bloqueado
 
 `.trim(), null, {
         mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
