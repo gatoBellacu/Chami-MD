9e06338e0e28342b0b451612a9d9743baf576fb6
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 16:21:57 -0600

    Update cmd-lock.js

--- plugins/cmd-lock.js
+++ plugins/cmd-lock.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!m.quoted) throw 'Tag Pesan!'
-    if (!m.quoted.fileSha256) throw 'Falta hash SHA256'
+    if (!m.quoted) throw '*⚠️ Etiqueta el mensaje!*'
+    if (!m.quoted.fileSha256) throw '*⚠️ Falta hash SHA256*'
     let sticker = db.data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
-    if (!(hash in sticker)) throw 'Hash no encontrado en la base de datos'
+    if (!(hash in sticker)) throw '*⚠️ Hash no encontrado en la base de datos*'
     sticker[hash].locked = !/^un/i.test(command)
-    m.reply('hecho!')
+    m.reply('*✅ hecho!*')
 } 
 handler.help = ['un', ''].map(v => v + 'lockcmd')
 handler.tags = ['database']
