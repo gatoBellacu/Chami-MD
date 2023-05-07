37e5495a85b776a54e62c12e7edb32d8920fbddf
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 16:20:06 -0600

    Create cmd-lock.js

---
+++ plugins/cmd-lock.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!m.quoted) throw 'Tag Pesan!'
+    if (!m.quoted.fileSha256) throw 'Falta hash SHA256'
+    let sticker = db.data.sticker
+    let hash = m.quoted.fileSha256.toString('hex')
+    if (!(hash in sticker)) throw 'Hash no encontrado en la base de datos'
+    sticker[hash].locked = !/^un/i.test(command)
+    m.reply('hecho!')
+} 
+handler.help = ['un', ''].map(v => v + 'lockcmd')
+handler.tags = ['database']
+handler.command = /^(un)?lockcmd$/i
+
+export default handler
