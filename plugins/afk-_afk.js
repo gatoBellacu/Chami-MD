e9d87a6835ad251de875a0304bdcd24065ffa501
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 15:15:28 -0600

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -4,11 +4,10 @@ export function before(m) {
     let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
         m.reply(`
-╭━─━─━─≪☣️≫─━─━─━╮
-┃𝙳𝙴𝙹𝙰𝚂𝚃𝙴 𝙳𝙴 𝙴𝚂𝚃𝙰 𝙰𝙵𝙺
-┃${user.afkReason ? '\n┃🔸️ *𝚁𝙰𝚉𝙾𝙽 :* ' + user.afkReason : ''}
-┃🔸 *𝙴𝚂𝚃𝚄𝚅𝙾 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 𝙳𝚄𝚁𝙰𝙽𝚃𝙴* ${(new Date - user.afk).toTimeString()}
-╰━─━─━─≪☣️≫─━─━─━╯`.trim())
+*⚠️ Dejaste de estar afk*
+
+📍 *Estuviste inactivo por:* ${(new Date - user.afk).toTimeString()}
+`.trim())
         user.afk = -1
         user.afkReason = ''
     }
@@ -22,11 +21,11 @@ export function before(m) {
             continue
         let reason = user.afkReason || ''
         m.reply(`
-💤 𝙽𝙾 𝙻𝙾𝚂 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 💤
-𝙴𝚜𝚝𝚎 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚚𝚞𝚎 𝚖𝚎𝚗𝚌𝚒𝚘𝚗𝚊𝚜 𝚎𝚜𝚝𝚊 𝙰𝙵𝙺  
+*⚠️ No lo etiquetes ⚠️*
+*Este usuario que mencionaste está afk*
 
-${reason ? '🔸️ *𝚁𝙰𝚉𝙾𝙽* : ' + reason : '🔸️ *𝚁𝙰𝚉𝙾𝙽* : 𝚂𝚒𝚗 𝚛𝚊𝚣𝚘𝚗'}
-🔸️ *𝙴𝚂𝚃𝚄𝚅𝙾 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 𝙳𝚄𝚁𝙰𝙽𝚃𝙴 :* ${(new Date - afkTime).toTimeString()}
+${reason ? '🔰 *Razon* : ' + reason : '🔰 *Razon* : sin razón xd'}
+📍 *Inactivo durante:* ${(new Date - afkTime).toTimeString()}
   `.trim())
     }
     return true
