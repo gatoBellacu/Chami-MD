d27455dead439613bb9c5a26b50aa36fc9d9a9e4
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 15:28:14 -0600

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -3,11 +3,11 @@
 let handler = async (m, { text }) => {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `✳️ Ingrese el nombre del comamdo`
+    if (!hash) throw `*⚠️ Ingrese el nombre del comamdo*`
     let sticker = global.db.data.sticker
-    if (sticker[hash] && sticker[hash].locked) throw '✳️ No puedes borrar este comando'
+    if (sticker[hash] && sticker[hash].locked) throw '*⚠️ No puedes borrar este comando*'
     delete sticker[hash]
-    m.reply(`✅ Comando eliminado`)
+    m.reply(`*✅ Comando eliminado*`)
 }
 
 
