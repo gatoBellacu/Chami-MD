4dedc827a36a40ee16423146ce9bf3ce4f2fe1c7
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 16:02:54 -0600

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -7,7 +7,7 @@ let user = global.db.data.users[m.sender]
 //let thumb = await getBuffer(global.pic)
 user.afk = + new Date
 user.afkReason = text
- conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, name, 'No moloestar ⚠️', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
+ sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, name, 'No moloestar ⚠️', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
         mediaUrl: dygp,
         mediaType: "VIDEO",
         description: dygp, 
