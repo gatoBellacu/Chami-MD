f91421e44bb5ad9c7c02eecefa0d970affe315e7
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-07 18:05:48 -0600

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,4 +1,80 @@
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import yts from 'yt-search'
+var handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `Use example ${usedPrefix}${command} naruto blue bird`
+  await m.reply(wait)
+  let search = await yts(text)
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
+  if (!search) throw 'Video Not Found, Try Another Title'
+  let { title, thumbnail, timestamp, views, ago, url } = vid
+
+  let captvid = `╭──── 〔 Y O U T U B E 〕 ─⬣
+⬡ Título: ${title}
+⬡ Duración: ${timestamp}
+⬡ Vistas: ${views}
+⬡ Subido: ${ago}
+⬡ Enlace: ${url}
+╰────────⬣`
+  conn.sendButton(m.chat, `╭──── 〔 Y O U T U B E 〕 ─⬣
+⬡ Título: ${title}
+⬡ Duración: ${timestamp}
+⬡ Vistas: ${views}
+⬡ Subido: ${ago}
+⬡ Enlece: ${url}
+╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['📽 VIDEO', `${usedPrefix}getvid ${url} 360`], false, { quoted: m, 'document': { 'url':'https://wa.me/12522518391' },
+'mimetype': global.dpdf,
+'fileName': `𝕐𝕠𝕦𝕋𝕦𝕓𝕖 ℙ𝕝𝕒𝕪𝕤`,
+'fileLength': 666666666666666,
+'pageCount': 666,contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType:  2,
+mediaUrl: `${url}`,
+title: `AUDIO SEDANG DIKIRIM...`,
+body: wm,
+sourceUrl: 'http://wa.me/12522518391', thumbnail: await ( await conn.getFile(thumbnail)).data
+  }
+ } 
+})
+  
+  //let buttons = [{ buttonText: { displayText: '📽VIDEO' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
+ //let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
+
+  const yt = await youtubedlv2(url).catch(async _ => await youtubedl(url))
+const link = await yt.audio['128kbps'].download()
+  let doc = { 
+  audio: 
+  { 
+    url: link 
+}, 
+mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType:  2,
+mediaUrl: url,
+title: title,
+body: wm,
+sourceUrl: url,
+thumbnail: await(await conn.getFile(thumbnail)).data                                                                     
+                                                                                                                 }
+                       }
+  }
+
+  return conn.sendMessage(m.chat, doc, { quoted: m })
+	// return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
+	// return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
+}
+handler.help = ['play'].map(v => v + ' <pencarian>')
+handler.tags = ['downloader']
+handler.command = /^play$/i
+
+handler.exp = 0
+handler.limit = true
+
+export default handler
+
+
+
+
+
+
+/*import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 	
 	if (!text) throw `*⚠️ Ingresa el título de una canción*\n\n*📌 Ejemplo ${usedPrefix + command}* Perfect`
@@ -24,4 +100,4 @@ handler.help = ['play']
 handler.tags = ['dl']
 handler.command = ['play', 'playvid']
 
-export default handler
+export default handler*/
