7099e691610e48b0d73e024bc3a0a653dde14e1b
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-08 17:20:52 -0300

    Add files via upload

--- plugins/econ-mine.js
+++ plugins/econ-mine.js
@@ -1,18 +1,33 @@
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn }) => {
+const fkontak = {
+        "key": {
+        "participants":"0@s.whatsapp.net",
+            "remoteJid": "status@broadcast",
+            "fromMe": false,
+            "id": "Halo"    
+        },
+        "message": {
+            "contactMessage": {
+                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+            }
+        },
+        "participant": "0@s.whatsapp.net"
+    }
 
-  let hasil = Math.floor(Math.random() * 1000)
-  let time = global.db.data.users[m.sender].lastmiming + 14400000
-  if (new Date - global.db.data.users[m.sender].lastmiming < 14400000) throw `⏳ _Espera_ *${msToTime(time - new Date())}* _para regresar a la mina_`
+let user = global.db.data.users[m.sender]
+  let hasil = Math.floor(Math.random() * 5000)
+  let time = global.db.data.users[m.sender].lastmiming + 300000
+  if (new Date - global.db.data.users[m.sender].lastmiming < 300000) throw `⏳ 𝔼𝕤𝕡𝕖𝕣𝕒 *${msToTime(time - new Date())}* ℙ𝕒𝕣𝕒 𝕧𝕠𝕝𝕧𝕖𝕣 𝕒 𝕞𝕚𝕟𝕒𝕣`
   global.db.data.users[m.sender].exp += hasil
-  m.reply(`
-conn.fakeReply(m.chat, '*⛏️ Genial! Minaste ${hasil}*', '0@s.whatsapp.net', '*🍧 CuriosityBot-MD 🍧*', 'status@broadcast')`)
+  m.reply(`⛏️ 𝔾𝕖𝕟𝕚𝕒𝕝 𝕞𝕚𝕟𝕒𝕤𝕥𝕖 ${hasil} 𝕏ℙ`)
   global.db.data.users[m.sender].lastmiming = new Date * 1
 }
 handler.help = ['mine']
 handler.tags = ['econ']
 handler.command = ['minar', 'miming', 'mine'] 
+handler.register = true
 
 export default handler
 
@@ -28,3 +43,4 @@ function msToTime(duration) {
 
   return hours + " hora(s) " + minutes + " minuto(s) " + seconds + " segundo(s)" 
 }
+
