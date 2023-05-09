1a72759af9a45879a700e51b92874c14a2f5d32b
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 10:15:12 -0600

    Update dl-yts.js

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -1,5 +1,32 @@
 import yts from 'yt-search'
 
+var handler = async(m, { conn, usedPrefix, text, args, command }) => {
+let name = await conn.getName(m.sender)
+
+  if (!text) throw 'Qué estás buscando?'
+  let cari = await yts(`${text}`)
+    let dapet = cari.all
+    let listSections = []
+	Object.values(dapet).map((v, index) => {
+	listSections.push([index + ' ' + cmenub + ' ' + v.title, [
+          ['Video 🎥', usedPrefix + 'getvid ' + v.url, '\n⌚ *Duración:* ' + v.timestamp + '\n⏲️ *Subido:* ' + v.ago + '\n👁️ *Vistas:* ' + v.views + '\n📎 *Url:* ' + v.url],
+          ['Audio 🎧', usedPrefix + 'getaud ' + v.url, '\n⌚ *Duración:* ' + v.timestamp + '\n⏲️ *Subido:* ' + v.ago + '\n👁️ *Vistas:* ' + v.views + '\n📎 *Url:* ' + v.url]
+        ]])
+	}) 
+	return conn.sendList(m.chat, '*───「 Youtube Search 」───*', `Seleccione un tipo a continuación...\n*El texto que solicitaste:* ${text}\n\Vuelva a escribir *${usedPrefix + command}* su texto para cambiar el texto de nuevo`, wm2, `YouTube Search 🔎`, listSections, m)
+}
+handler.help = ['ytsearch <query>']
+handler.tags = ['internet']
+handler.command = /^yts(earch)?$/i
+
+
+export default handler
+
+
+
+
+/*import yts from 'yt-search'
+
 let handler = async (m, {conn, text }) => {
   if (!text) throw '*⚠️ Que quieres que busque en YouTube?*'
   let results = await yts(text)
@@ -27,4 +54,4 @@ handler.help = ['ytsearch']
 handler.tags = ['dl']
 handler.command = ['ytsearch', 'yts'] 
 
-export default handler
+export default handler*/
