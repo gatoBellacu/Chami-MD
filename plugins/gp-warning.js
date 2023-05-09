8869bca33296b9ea99777dc78e942812ec8bb6d9
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 07:27:46 -0600

    Update gp-warning.js

--- plugins/gp-warning.js
+++ plugins/gp-warning.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
         if (!who) throw `*⚠️ Etiqueta o menciona a alguien*\n\n*🔰 Ejemplo :* ${usedPrefix + command} @user`
-        if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+        if (!(who in global.db.data.users)) throw `*⚠️ El usuario no se encuentra en mi base de datos*`
         let name = conn.getName(m.sender)
         let warn = global.db.data.users[who].warn
         if (warn < war) {
