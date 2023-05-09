5857d10b0b3f6c7274f206c02765b2403ead7648
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 08:29:26 -0600

    Update img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*⚠️ Que imagen quieres que busque?*\n\n*🔰 Ejemplo  :* ${usedPrefix + command} Lil Peep`
   const json = await pinterest(text)
   conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
-*🔰  Pinterest:*  ${text}
+*🧩 Pinterest:*  ${text}
 `.trim(), m)
 }
 handler.help = ['pinterest']
