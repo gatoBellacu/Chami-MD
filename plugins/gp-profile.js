400f903b239d2d63cc26602832b5ee47c26f5551
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-08 20:35:25 -0600

    Update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -6,7 +6,7 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn, usedPrefix, command}) => {
 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+if (!(who in global.db.data.users)) throw `*⚠️ El usuario no se encuentra en mi base de datos*`
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
 let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
@@ -18,18 +18,18 @@ let sn = createHash('md5').update(who).digest('hex')
 
 let str = `
 ┌───「 *PERFIL* 」
-▢ *🔖 Nombres:* 
+→ *🔖 Nombres:* 
    • ${username} ${registered ? '\n   • ' + name + ' ': ''}
    • @${who.replace(/@.+/, '')}
-▢ *📱Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-▢ *🔗Link:* wa.me/${who.split`@`[0]}${registered ? '\n▢ *🎈Edad*: ' + age + ' años' : ''}
-▢ *⚠️Advertencias:* ${warn}/${maxwarn}
-▢ *💎 Diamantes :* ${diamond}
-▢ *🆙 Nivel* : ${level}
-▢ *⬆️ XP* : Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `listo para *${usedPrefix}levelup*` : `_*${math}xp*_ Falta para subir de nivel`}
-▢ *🏆Rango:* ${role}
-▢ *📇 Registrado :* ${registered ? 'Si': 'No'}
-▢ *⭐ Premium* : ${prem ? 'Si' : 'No'}
+→ *📱Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+→ *🔗 Link:* wa.me/${who.split`@`[0]}${registered ? '\n*🎈 Edad*: ' + age + ' años' : ''}
+→ *⚠️ Advertencias:* ${warn}/${maxwarn}
+→ *💎 Diamantes :* ${diamond}
+→ *🆙 Nivel* : ${level}
+→ *⬆️ XP* : Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `listo para *${usedPrefix}levelup*` : `_*${math}xp*_ Falta para subir de nivel`}
+→ *🏆 Rango:* ${role}
+→ *📇 Registrado :* ${registered ? 'Si': 'No'}
+→ *⭐ Premium* : ${prem ? 'Si' : 'No'}
 └──────────────`
     conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
     m.react(done)
