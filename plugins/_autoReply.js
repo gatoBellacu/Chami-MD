0cdf07fce3e74951e4a12023c2b6c56394f84aa4
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 14:33:38 -0600

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -2,6 +2,8 @@ export async function all(m) {
 	
     // cuando alguien envía un enlace de un grupo al dm del bot
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Abre este enlace')) && !m.isBaileys && !m.isGroup) {
+	   let fkontak2 = { key: { participant: '0@s.whatsapp.net' }, message: { contactMessage: { displayName: packname, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${author},;;;\nFN:${author},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, jpegThumbnail: fs.readFileSync('./media/menus/Menu1.jpg'), thumbnail: fs.readFileSync('./media/menus/Menu1.jpg'), sendEphemeral: true }}}
+
      this.sendButton(m.chat, `*🍧 INVITAR BOT A SU GRUPO 🍧* 
         
  👋🏻 Hola @${m.sender.split('@')[0]}
