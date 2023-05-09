15585b845cd729a0afd41e3dcc03fad8b6fbc8a7
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 14:21:03 -0600

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-//l
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
  
 conn.sendButton(m.chat, `*📑 LISTA CMD 📑*
 \`\`\`
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n\n')}
-\`\`\``, null, [['✨ MENU ✨', `/menu`]], fdoc, { contextInfo: { mentionedJid: [who], forwardingScore: 999, isForwarded: true}})
+\`\`\``, null, [['✨ MENU ✨', `/menu`]], fdc, { contextInfo: { mentionedJid: [who], forwardingScore: 999, isForwarded: true}})
 }
 handler.help = ['listcmd']
 handler.tags = ['database']
