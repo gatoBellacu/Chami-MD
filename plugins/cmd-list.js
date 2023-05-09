5cc2ad8da651ba0e55fc87437b1865cd2c6150c0
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 14:10:08 -0600

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,4 +1,5 @@
 let handler = async (m, { conn }) => {
+let name = conn.getName(who)
 let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${wm2}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${wm2}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
    
 conn.sendButton(m.chat, `*🍧 LISTA CMD 🍧*
