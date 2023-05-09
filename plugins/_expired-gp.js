e33aa46742cf8d1a7d06678fd12d487f90b26d38
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 12:57:51 -0600

    Update _expired-gp.js

--- plugins/_expired-gp.js
+++ plugins/_expired-gp.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, `*👋🏻 Adios *${this.user.name}* saldré del grupo*\n\n*🔰 finalizo su invitación 🔰*`)
+        await this.reply(m.chat, `*👋🏻 Adios ${this.user.name} saldré del grupo*\n\n*🔰 finalizo su invitación 🔰*`)
         await this.groupLeave(m.chat)
         chats.expired = null
     }
