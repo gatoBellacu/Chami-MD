08b57fc2699605aacaf28d524691e29601ef0dd6
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-08 20:25:44 -0600

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,13 +1,13 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 	
-let kickte = `✳️ Uso correcto del comamdo\n*${usedPrefix + command}* @tag`
+let kickte = `*⚠️ Uso correcto del comamdo*\n*${usedPrefix + command}* @tag`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ Usuario eliminado`) 
+m.reply(`*✅ Usuario eliminado*`) 
 
 }
 
