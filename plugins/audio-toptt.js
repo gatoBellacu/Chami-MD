2bf6c4eb4362b35ef99f812e886df790bda6aa44
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 13:09:37 -0600

    Update audio-toptt.js

--- plugins/audio-toptt.js
+++ plugins/audio-toptt.js
@@ -3,7 +3,7 @@ import { toPTT } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `*⚠️ Responda el audio que desea convertir a nota de voz con :*\n *${usedPrefix + command}*`
+    if (!/video|audio/.test(mime)) throw `*⚠️ Responda el audio que desea convertir a nota de voz con:*\n${usedPrefix + command}`
     let media = await q.download?.()
     if (!media) throw '*⚠️ Error al descargar medios*'
     let audio = await toPTT(media, 'mp4')
