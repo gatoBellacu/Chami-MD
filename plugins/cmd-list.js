f951c4d3f9872bfc140cd1b7d0d702561ca90570
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-05-09 14:17:40 -0600

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${wm2}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${wm2}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
-   
+//let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${wm2}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${wm2}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: wm}}}}
+ 
 conn.sendButton(m.chat, `*📑 LISTA CMD 📑*
 \`\`\`
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n\n')}
-\`\`\``, null, [['✨ MENU ✨', `/menu`]], fkon, { contextInfo: { mentionedJid: [who], forwardingScore: 999, isForwarded: true}})
+\`\`\``, null, [['✨ MENU ✨', `/menu`]], fdoc, { contextInfo: { mentionedJid: [who], forwardingScore: 999, isForwarded: true}})
 }
 handler.help = ['listcmd']
 handler.tags = ['database']
