d1a3685250f2389960a090d2ad1bf54a1d59166d
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-12 15:32:52 -0300

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -12,8 +12,12 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return m.reply('[ ! ] Aea causa acabas de enviar un enlace, lo weno es que el enlace detectado es de este grupo owo')
         }
-        if (!isBotAdmin) return conn.sendButton(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n`, `Por suerte no soy acmin, asi que no puedo hacer nada :v`, wm, ['[ DESACTIVAR ANTILINK ]', usedPrefix+'disable antilink'], m)
-        await  conn.sendMessage(m.chat, { text: `*⚠️ ENLACE DETECTADO ⚠️*\n\n*Valiste vrg @${m.sender.split("@")[0]} Adios..._\n`, mentions: [m.sender] }) 
+        if (!isBotAdmin)
+      await conn.reply(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\nPor suerte no soy acmin, asi que no puedo hacer nada :v`, null, { mentions: [aa] })
+if (isBotAdmin && bot.restrict) {
+await conn.reply(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n\n*Valiste vrg @${m.sender.split("@")[0]} Adios..._\n`, null, { mentions: [aa] })}
+        /*if (!isBotAdmin) return conn.sendButton(m.chat, `*⚠️ ENLACE DETECTADO ⚠️*\n`, `Por suerte no soy acmin, asi que no puedo hacer nada :v`, wm, ['[ DESACTIVAR ANTILINK ]', usedPrefix+'disable antilink'], m)
+        await  conn.sendMessage(m.chat, { text: `*⚠️ ENLACE DETECTADO ⚠️*\n\n*Valiste vrg @${m.sender.split("@")[0]} Adios..._\n`, mentions: [m.sender] }) */
         conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: yid, participant: prt }})
 setTimeout(() => { 
 	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
