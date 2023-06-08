2479ca2cea9a162f63a72d8556bbced68acec851
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-08 00:48:52 -0600

    Update propietario(a)-restart.js

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -2,10 +2,10 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (global.conn.user.jid == conn.user.jid) {
-    await m.reply('🚀🚀')
-         await m.reply('🚀🚀🚀🚀')
-         await m.reply('🚀🚀🚀🚀🚀🚀')
-         await m.reply('𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩') 
+    await m.reply('🚀')
+         await m.reply('🚀🚀')
+         await m.reply('🚀🚀🚀')
+         await m.reply('*💻 REINICIADO*') 
     process.send('reset')
   } else throw '_eeeeeiiittsssss..._'
 }
