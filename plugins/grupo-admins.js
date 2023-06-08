89a06a4d29d92a7241b687f2a155d20aa8296163
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-07 20:24:00 -0600

    Update grupo-admins.js

--- plugins/grupo-admins.js
+++ plugins/grupo-admins.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './storage/img/admins.jpg'
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './storage/menus/Menu2.jpg'
 const groupAdmins = participants.filter(p => p.admin)
 const listaAdmins = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
