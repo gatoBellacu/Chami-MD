d921dff4c37d5c6c0c6445f6907cdd707ca752b6
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-07 19:42:32 -0600

    Update descargas-ytv.js

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -2,7 +2,7 @@ import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!args[0]) return conn.reply(m.chat, `*⚠️ INGRESE UN ENLACE DE YOUTUBE PARA DESCARGAR EL VIDEO*\n\n🔰 EJEMPLO\n*${usedPrefix + command} https://youtu.be/85xI8WFMIUY*`, fkontak, m)
+if (!args[0]) return conn.reply(m.chat, `*⚠️ INGRESE UN ENLACE DE YOUTUBE PARA DESCARGAR EL VIDEO*\n\n💡 EJEMPLO\n*${usedPrefix + command} https://youtu.be/85xI8WFMIUY*`, fkontak, m)
 await conn.reply(m.chat, `*🚀 SE ESTA DESCARGANDO SU VÍDEO, ESPERE UN MOMENTO*`, fkontak, m)
 try {
 let qu = args[1] || '360'
