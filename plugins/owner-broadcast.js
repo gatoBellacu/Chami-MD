ac00a502ec9c33bd6d5e01b17aef3241bbd57458
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-12 18:26:55 -0600

    Update owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -3,25 +3,25 @@
 import { randomBytes } from 'crypto'
 
 let handler = async (m, { conn, command, participants, usedPrefix, text }) => { 
-if (!text) return '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙴 𝚃𝚁𝙰𝙼𝙸𝚃𝙴*' 
+if (!text) return '*⚠️ INGRESE EL TEXTO QUE QUIERE QUE TRÁMITE*' 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" } 
 let cc4 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks4 = text ? text : cc4.text 
 let groups2 = Object.keys(await conn.groupFetchAllParticipating())
 let chats2 = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net'))
-await conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*', m)
+await conn.reply(m.chat, '*🚀 ENVIANDO MENSAJE A TODOS LOS CHATS*\n\n*🔔NOTA: ES POSIBLE QUE NO SE ENVIE A TODOS LOS CHATS*', m)
 let start2 = new Date().getTime()
 let usersTag2 = participants.map(u => conn.decodeJid(u.id))
 let totalPri2 = 0
 for (let i = 0; i < groups2.length; i++) {
 const group = groups2[i];
-const delay = i * 4000; // 4 seg
+const delay = i * 6000; // 6 seg
 setTimeout(async () => {
-await conn.reply(group, `*╔══❰ 𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎 ❱══╗*\n\n` + teks4, { mentions: usersTag2 }, { quoted: fkontak });
+await conn.reply(group, `––––––『 *BROADCAST* 』––––––\n\n` + teks4, { mentions: usersTag2 }, { quoted: fkontak });
 }, delay)}
 for (let user of chats2) {
-await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
-await conn.reply(user, `*╔══❰ 𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎 ❱══╗*\n\n` + teks4, fkontak, null)
+await new Promise(resolve => setTimeout(resolve, 5000)) // 5 segundos
+await conn.reply(user, `––––––『 *BROADCAST* 』––––––\n\n` + teks4, fkontak, null)
 totalPri2++
 if (totalPri2 >= 500000) { 
 break
@@ -38,10 +38,10 @@ time2 = `${minutes} minutos y ${seconds} segundos`
 } else {
 time2 = `${time2} segundos`
 } 
-await m.reply(`𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰:
-\`\`\`𝙲𝙷𝙰𝚃 𝙿𝚁𝙸𝚅𝙰𝙳𝙾 >> ${totalPrivate2}\`\`\`
-\`\`\`𝙲𝙷𝙰𝚃 𝙶𝚁𝚄𝙿𝙾𝚂  >>   ${totalGroups2}\`\`\`
-\`\`\`𝙲𝙷𝙰𝚃𝚂 𝚃𝙾𝚃𝙰𝙻𝙴𝚂  >>   ${total2}\`\`\`\n\n*𝚃𝙸𝙴𝙼𝙿𝙾 𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝙴𝙽𝚅𝙸𝙾 ${time2}*\n${totalPri2 >= 500000 ? `\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*` : ''}`)        
+await m.reply(`📑 MENSAJE ENVIADO A:
+\`\`\`Chats privados >> ${totalPrivate2}\`\`\`
+\`\`\`Chats grupales >>   ${totalGroups2}\`\`\`
+\`\`\`Chats totales  >>   ${total2}\`\`\`\n\n*𝚃𝙸𝙴𝙼𝙿𝙾 𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝙴𝙽𝚅𝙸𝙾 ${time2}*\n${totalPri2 >= 500000 ? `\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*` : ''}`)        
 }
 handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
