8910fe99160b37b1e4c75eb0a6b70d430c09026c
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-13 20:07:46 -0600

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -10,20 +10,20 @@ let handler = async (m, { conn }) => {
     let name = conn.getName(who)
     let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let str = `
-]──────────❏ *PROFILE* ❏──────────[
+]──────────❏ *PERFIL* ❏──────────[
 💌 • *Nombre:* ${username} 
 🎐 • *Username:* ${registered ? name : ''}
 📧 • *Tag:* @${who.replace(/@.+/, '')}
 📞 • *Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 🔗 • *Link:* https://wa.me/${who.split`@`[0]}
 🎨 • *Edad:* ${registered ? age : ''}
-${readMore}
 🌟 • *Premium:* ${premium ? "✅" :"❌"}
 ⏰ • *PremiumTime:* 
 ${clockString(user.premiumTime)}
 📑 • *Registrado:* ${registered ? '✅': '❌'}
 `.trim()
-  
+ conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
+    m.react(🌟) 
 }
 handler.help = ['profile [@user]']
 handler.tags = ['exp']
