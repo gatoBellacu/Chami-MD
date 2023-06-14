cbad0c83309389ca7d03382247028be7f8666a81
Azami19 <119384234+Azami19@users.noreply.github.com>
2023-06-13 19:51:50 -0600

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -1,27 +1,17 @@
-let handler = async (m, { conn, isPrems}) => { //lastmiming
-let minar = `${pickRandom(['Que pro 😎 has minado','🌟✨ Genial!! Obtienes','WOW!! eres un(a) gran Minero(a) ⛏️ Obtienes','Felicidades!! Ahora tienes','⛏️⛏️⛏️ Obtienes'])}`
-let pp = 'https://media.istockphoto.com/vectors/basic-rgb-vector-id1315251368?b=1&k=6&m=1315251368&s=170667a&w=0&h=2BgQx5Pu2CewGeq93Qxsyoyw5oT4gioHOOIkHb7PoyY='
-
-let xp = Math.floor(Math.random() * 2000)
-global.db.data.users[m.sender].exp += xp * 1  
-//let hasil = Math.floor(Math.random() * 2000)
+let handler = async (m, { conn, isPrems}) => {
+let hasil = Math.floor(Math.random() * 1000)
+let info = `*Genial minaste ${hasil} Diamantes 💎*`
 let time = global.db.data.users[m.sender].lastmiming + 600000
-if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `*💟 Vuelva en ${msToTime(time - new Date())} para continuar minando ⛏️*`  
+if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `*⏰ Debes esperar ${msToTime(time - new Date())} para volver a minar*`  
 
-conn.sendHydrated(m.chat, `*${minar} ${xp} 𝙓𝙋*`, wm, pp, md, '𝗡𝗘𝗞𝗢𝗕𝗢𝗧-𝗠𝗗', null, null, [
-['𝙈𝙞𝙣𝙖𝙧 𝘿𝙞𝙖𝙢𝙖𝙣𝙩𝙚𝙨 💎', `.minar3`],
-['𝙈𝙞𝙣𝙖𝙧 𝗡𝗲𝗸𝗼𝘾𝙤𝙞𝙣𝙨 🔮', `.minar2`],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `.menu`]
-], m,)
-global.db.data.users[m.sender].lastmiming = new Date * 1  
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🍁 CuriosityBot-MD 🍁*', 'status@broadcast')
+//m.reply(`*[ 🎉 ] 𝙶𝚎𝚗𝚒𝚊𝚕, 𝚖𝚒𝚗𝚊𝚜𝚝𝚎 ${hasil} 𝚇𝙿*`)
+global.db.data.users[m.sender].lastmiming = new Date * 1
   
-
-//m.reply(`*${minar} *${hasil} 𝙓𝙋*`)
-
 }
 handler.help = ['minar']
 handler.tags = ['xp']
-handler.command = ['minar', 'miming', 'mine', 'minarxp', 'minarexp', 'minarexperiencia'] 
+handler.command = ['minar', 'miming', 'mine'] 
 handler.fail = null
 handler.exp = 0
 export default handler
@@ -37,8 +27,4 @@ minutes = (minutes < 10) ? "0" + minutes : minutes
 seconds = (seconds < 10) ? "0" + seconds : seconds
 
 return minutes + " m y " + seconds + " s " 
-}  
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
-
+}
