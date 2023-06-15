fdf64d58ea51ff1a7f1b44ff8eb03053a8af1adf
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-14 21:02:15 -0300

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎 Curisitybug`
+		let bio = `\n🥏 Tiempo Activo ${muptime}\n\n | 💎 Curisitybug`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
