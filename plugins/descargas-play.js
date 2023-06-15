6b0b9ec3042adce4b0a27a65ce3764b7d68629b6
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-15 16:43:01 -0300

    Add files via upload

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,73 +1,62 @@
-import { youtubeSearch, youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper' 
-import fetch from "node-fetch"
 import yts from 'yt-search'
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-let q, v, yt, dl_url, ttl, size, lolhuman, lolh, n, n2, n3, n4, cap, qu, currentQuality
-	if (!text) throw `*⚠️ INGRESE EL NOMBRE DE LA CANCIÓN QUE ESTÁ BUSCANDO*\n\n💡 EJEMPLO\n*${usedPrefix + command} Another love*`
-	m.react('🎧') 
-	let vid = (await yts(text)).all[0]
-let { title, description, publishedTime, url, thumbnail, videoId, timestamp, views, published } = vid
-try {
-if (command == 'play') {	
-conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+import fetch from "node-fetch"
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
+let limit = 1500
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+  
+    if (!text) throw `*⚠️ INGRESE EL NOMBRE DE LA CANCIÓN QUE ESTÁ BUSCANDO*\n\n💡 EJEMPLO\n*${usedPrefix + command} Another love*`
+  let chat = global.db.data.chats[m.chat]
+  let res = await yts(text)
+  //let vid = res.all.find(video => video.seconds < 3600)
+  let vid = res.videos[0]
+  let { title, description, publishedTime, url, thumbnail, videoId, timestamp, views, published } = vid
+  if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+  let isVideo = /vid$/.test(command)
+  m.react('🎧') 
+  
+  try {
+  let q = isVideo ? '360p' : '128kbps' 
+  let v = vid.url
+  let yt = await youtubedl(v).catch(async () => await youtubedlv2(v)).catch(async () => await youtubedlv3(v))
+  let dl_url = await (isVideo ? yt.video[q].download() : yt.audio[q].download())
+  let title = await yt.title
+  let size = await (isVideo ? yt.video[q].fileSizeH : yt.audio[q].fileSizeH)
+  let play = `
 *📑 TÍTULO:*
-${title}
+${vid.title}
 
 *⏰ DURACIÓN:* 
-${timestamp}
+${vid.timestamp}
 
+*📆 PUBLICADO:* 
+${vid.ago}
+ 
 *💬 DESCRIPCIÓN*
 ${description}
 
 *👀 VISTAS*
-${views}
+${vid.views}
 
 *📡 URL*
 ${url}
 
-*🚀 Aguarde un momento en lo que envío su audio*
-`.trim(), m)
-q = '128kbps'
-v = url
-yt = await youtubedl(v).catch(async () => await youtubedlv2(v)).catch(async () => await youtubedlv3(v))
-dl_url = await yt.audio[q].download()
-title = await yt.title
-size = await yt.audio[q].fileSizeH
-await conn.sendFile(m.chat, dl_url, title + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+*🚀 Aguarde un momento en lo que envío su audio*`
+conn.sendFile(m.chat, vid.thumbnail, 'play', play, m, null)
+
+if (size.split('MB')[0] >= limit) return m.reply(`🔸 *⚖️Peso* : ${size}\n🔸 *🎞️Calidad* : ${q}\n\n🔸 _El archivo supera el límite de descarga_ *+${limit} MB*`) 
+if (size.includes('GB')) return m.reply(`🔸 *⚖️Peso* : ${size}\n🔸 *🎞️Calidad* : ${q}\n\n🔸 _El archivo supera el límite de descarga_ *+${limit} MB*`)   
+	  conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /vid$/.test(command)), `🔸 *📌Título* : ${title}
+🔸 *🎞️Calidad* : ${q}
+🔸 *⚖️Peso* : ${size}
+`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
+		m.react(done) 
+    } catch {
+		m.reply(`Error: intenta de nuevo`)
+    }
+
 }
-if (command == 'play2') {
-conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-*📑 TÍTULO:*
-${title}
-
-*⏰ DURACIÓN:* 
-${timestamp}
-
-*💬 DESCRIPCIÓN*
-${description}
-
-*👀 VISTAS*
-${views}
-
-*📡 URL*
-${url}
-
-*🚀 Aguarde un momento en lo que envío su video*
-`.trim(), m)
-let qu = args[1] || '360'
-let q = qu + 'p'
-let v = args[0]
-const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
-const dl_url = await yt.video[q].download()
-const ttl = await yt.title
-const size = await yt.video[q].fileSizeH
-m.react(done)
-await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*🔰 Aqui esta tu video*\n*🔥 Titulo: ${ttl}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
-}
-} catch (e) {
-}}
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
-handler.command = ['play', 'play2']
+handler.help = ['play']
+handler.tags = ['dl']
+handler.command = ['play', 'playvid']
 
 export default handler
