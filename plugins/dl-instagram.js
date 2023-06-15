08fa0ef608d7987b28ce300f3da0e85b2142492a
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-15 14:40:59 -0300

    Add files via upload

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -2,9 +2,9 @@
 import fg from 'api-dylux'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `·˚ ༘₊· ͟͟͞͞꒰➳Uso del comando\nEjemplo*${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+    if (!args[0]) throw `·˚ ༘₊· ͟͟͞͞꒰➳ 𝚄𝚜𝚘 𝚍𝚎𝚕 𝚌𝚘𝚖𝚊𝚗𝚍𝚘\n𝙴𝚓𝚎𝚖𝚙𝚕𝚘: *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
     m.react(rwait)
-    m.reply('Calmao 😎\n\n*Estoy descargando tu post 🔄*\n\nAguarde un momento, por favor')
+    m.reply('Calmao 😎\n*Estoy descargando tu post 🔄*\n\nAguarde un momento, por favor')
     let res = await fg.igdl(args[0])
     for (let result of res.url_list) {
     conn.sendFile(m.chat, result, 'igdl.mp4', `『 Resultado 』`, m)
--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -2,10 +2,10 @@
 import fg from 'api-dylux' 
 import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!args[0]) throw `➳ Ingrese un link de Tiktok\n\n 📌 Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`
+if (!args[0]) throw `*➳ INGRESE UN LINK DE TIKTOK*\n\n*📌 EJEMPLO:*\n${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`
 if (!args[0].match(/tiktok/gi)) throw `❎ verifica que el link sea de tiktok`
 m.react(rwait)
-m.reply('Calma ✋🥸🤚\n\n*Estoy descargando tu video de tiktok 🔄*\n\nAguarde un momento, por favor*')
+m.reply('Calma ✋🥸🤚\n\n*Estoy descargando tu video de tiktok 🔄*\n\n*Aguarde un momento, por favor*')
 try {
     let p = await fg.tiktok(args[0]) 
     let te = `╭━─━─━─≪TIKTOK≫─━─━─━╮
