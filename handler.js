20e58392b303a9ec72f75262a5ccf143d0d6ebfc
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-14 22:43:32 -0300

    Add files via upload

--- handler.js
+++ handler.js
@@ -684,7 +684,7 @@ contextInfo: {
 "thumbnail": apii.data,
 "mediaUrl": 'https://youtu.be/EaXoIuT3UQ0',
 "sourceUrl": 'https://www.xvideos.com' }}} 
-this.sendMessage(id, pp, 'pp.jpg', buttonMessage, fake)                 
+this.sendMessage(id, pp, buttonMessage, fake) 
 //this.sendFile(id, apii.data, 'pp.jpg', text, buttonMessage, fake, null, false, { mentions: [user] }) 
 //this.sendMessage(id, apii.data, 'pp.jpg', buttonMessage, fake)                          
 }}}
