4cc6897db32a590fafeb7533789382fe18ce5df2
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-15 22:07:41 -0300

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -6,7 +6,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
+let pp = await conn.profilePictureUrl(who).catch(_ => imagen1())
 let name = await conn.getName(who)
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
